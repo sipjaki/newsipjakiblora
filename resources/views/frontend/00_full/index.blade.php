cf5c755f112f78ac620d1fc0795a8277790f4b04
Sigimatika <sigitechindo@gmail.com>
2024-07-17 02:20:01 +0700

    f

--- resources/views/frontend/00_full/index.blade.php
+++ resources/views/frontend/00_full/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../bahan1/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="../bahan1/css/animate.compat.css" rel="stylesheet" type="text/css">
-    <link href="../bahan1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="../bahan1/css/styles.css" rel="stylesheet" type="text/css">
+    <link href="./bahan1/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="./bahan1/css/animate.compat.css" rel="stylesheet" type="text/css">
+    <link href="./bahan1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="./bahan1/css/styles.css" rel="stylesheet" type="text/css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
