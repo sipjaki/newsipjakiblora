676d629d61ca1e52a81d098cce4021cf86a2aba6
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-07-26 14:25:03 +0700

    d

--- resources/views/frontend/01_profil/01_struktur.blade.php
+++ resources/views/frontend/01_profil/01_struktur.blade.php
@@ -55,8 +55,10 @@ Struktur Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Bandung Barat
     align-items: center;
     position: relative;
 ">
-    <img src="/assets/icon/pupr.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; padding: 0; margin-top: 10px;">
-    <img src="/assets/icon/sipjakikbb.png" alt="Logo SIPJAKIKBB" style="margin-bottom: 10px; width: 58px; height: 58px; object-fit: cover; margin-right: 10px; padding: 0;">
+   <div>
+    <img src="/assets/icon/pupr.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; float: left; margin: 0 10px;">
+    <img src="/assets/icon/sipjakikbb.png" alt="Logo SIPJAKIKBB" style="width: 58px; height: 58px; object-fit: cover; float: left; margin: 0 10px;">
+</div>
 
     <h1 style="margin-top:10px; font-size: 16px; font-family: 'Lato', sans-serif; font-weight: 700;">Kementrian Pekerjaan Umum Dan Penataan Ruang </h1>
     <div class="container" style="margin-top: 20px;">
