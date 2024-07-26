9935c3d2c40eb6854c0cb48713be60dc996635d0
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-07-26 16:27:57 +0700

    sdfgsdfg

--- app/Http/Controllers/SkktenagakerjaController.php
+++ app/Http/Controllers/SkktenagakerjaController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 
-use App\Models\Skktenagakerja; // Pastikan namespace model sesuai dengan struktur direktori
+use App\Models\skktenagakerja; // Pastikan namespace model sesuai dengan struktur direktori
 
 class SkktenagakerjaController extends Controller
 {
     public function index()
     {
-        $data= Skktenagakerja::paginate(15); // Menggunakan paginate() untuk pagination
+        $data= skktenagakerja::paginate(15); // Menggunakan paginate() untuk pagination
 
         return view('backend.04_skk.01_skk.index', [
             'title' => 'SKK Tenaga Kerja',
