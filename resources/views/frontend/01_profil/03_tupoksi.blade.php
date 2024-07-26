df0443ef94f44b66be00eaee03e11f303645e220
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-07-26 11:39:04 +0700

    s

--- resources/views/frontend/01_profil/03_tupoksi.blade.php
+++ resources/views/frontend/01_profil/03_tupoksi.blade.php
@@ -49,7 +49,7 @@ Tupoksi Seksi Program Dan Jasa Konstruksi
     border-radius: 25px;
     text-align: center;
     width: 100%;
-    height: 150vh;
+    height: 200vh;
     margin-left: 150px;
     background: linear-gradient(to bottom, yellow, white, black);
     align-items: center;
