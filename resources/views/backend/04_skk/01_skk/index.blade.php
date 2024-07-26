117473118a871672c5d619bb676363b09d0e2458
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-07-26 14:46:55 +0700

    d

--- resources/views/backend/04_skk/01_skk/index.blade.php
+++ resources/views/backend/04_skk/01_skk/index.blade.php
@@ -20,6 +20,112 @@
                         <div class="clearfix"></div>
                     </div>
                 </div>
+                <div class="container" style="
+    display: flex;
+    flex-direction: column;
+    background-color: #FFCB0F;
+    padding: 10px;
+    border: 1px solid black;
+    margin-bottom: 0px;
+    border-radius: 25px;
+    text-align: center;
+    width: 100%;
+    height: 90vh;
+    margin-left: 150px;
+    background: linear-gradient(to bottom, yellow, white, black);
+    align-items: center;
+    position: relative;
+">
+<div style="display: flex; align-items: center;">
+    <img src="/assets/icon/pupr.png" alt="Logo SIPJAKIKBB" style="width: 50px; height: 50px; object-fit: cover; margin: 0 10px;">
+    <img src="/assets/icon/sipjakikbb.png" alt="Logo SIPJAKIKBB" style="width: 70px; height: 70px; object-fit: cover; margin: 0 10px;">
+</div>
+
+
+    <h1 style="margin-top:10px; font-size: 16px; font-family: 'Lato', sans-serif; font-weight: 700;">Kementrian Pekerjaan Umum Dan Penataan Ruang </h1>
+    <div class="container">
+        <div class="card" style="
+            background-color: white;
+            border-radius: 20px;
+            padding: 20px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            max-width: 100%;
+            margin: 20px auto;">
+
+
+            {{-- ------------------------------------ FITUR TAMBAHAN ------------- --}}
+
+            <div class="controls" style="  
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 20px;
+            border-radius:5px;
+            ">
+<div class="show-entries" style="border-radius: 10px;">
+    <label for="entries" style="margin-right: 5px;">
+        <style>
+            .badge {
+                background: linear-gradient(to right, yellow, black);
+                color: white;
+                padding: 10px 20px;
+                border-radius: 10px;
+                display: inline-block;
+                font-size: 1rem;
+                margin-right: 10px;
+                text-align: center;
+                transition: background-color 0.3s, color 0.3s;
+            }
+    
+            .badge:hover {
+                background-color: white;
+                color: black;
+            }
+        </style>
+        <div class="badge"><i class="fas fa-edit me-2"></i>Sertifikat Keahlian </div></label>
+    {{-- <select id="entries" onchange="showEntries()">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+        </select> --}}
+</div>
+<div class="search-entries">
+    <style>
+        .search-container {
+            position: relative;
+            display: inline-block;
+        }
+        .search-container input {
+            border-radius: 15px;
+            padding: 2px 5px 2px 25px; /* Extra padding for the icon */
+        }
+        .search-container .fa-search {
+            position: absolute;
+            left: 10px;
+            top: 50%;
+            transform: translateY(-50%);
+        }
+    </style>
+    <div class="search-container">
+        <input
+            type="text"
+            id="searchInput"
+            onkeyup="searchEntries()"
+            placeholder="  Search..">
+        <i class="fas fa-search"></i>
+    </div>
+
+    <script>
+        function searchEntries() {
+            // Your JavaScript function for searching entries
+        }
+    </script>
+</div>
+</div>
+
+
 
 
                 
