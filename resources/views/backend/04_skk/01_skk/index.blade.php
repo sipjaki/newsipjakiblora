aff7bfed56cd7a5c0cc724ddf1c1c6d39d9d54fb
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-07-26 14:56:39 +0700

    dsfg

--- resources/views/backend/04_skk/01_skk/index.blade.php
+++ resources/views/backend/04_skk/01_skk/index.blade.php
@@ -31,7 +31,7 @@
     border-radius: 25px;
     text-align: center;
     width: 100%;
-    height: 90vh;
+    height: 200vh;
     margin-left: none;
     background: linear-gradient(to bottom, yellow, white, black);
     align-items: center;
