c9d9d5ea55e7c81cf5b1de734f9df164ec1edd90
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-07-28 11:05:03 +0700

    dfg

--- resources/views/backend/04_skk/01_skk/index.blade.php
+++ resources/views/backend/04_skk/01_skk/index.blade.php
@@ -253,34 +253,33 @@
 
         </div>
 
-        <div class="pagination-container" style="margin-top: 20px; display: flex; justify-content: center;">
-                                <ul class="pagination" style="display: flex; padding-left: 0; list-style: none;">
-                                    <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
-                                        <a class="page-link" href="{{ $data->previousPageUrl() }}" style="position: relative; display: block; padding: 0.5rem 0.75rem; margin-left: -1px; line-height: 1.25; color: #007bff; background-color: #fff; border: 1px solid #dee2e6;">Previous</a>
-                                    </li>
-                            
-                                    @foreach ($data->getUrlRange($data->currentPage() - 0, $data->currentPage() + 2) as $page => $url)
-                                        <li class="page-item {{ $page == $data->currentPage() ? 'active' : '' }}" style="margin-right: 5px;">
-                                            <a class="page-link" href="{{ $url }}" style="position: relative; display: block; padding: 0.5rem 0.75rem; margin-left: -1px; line-height: 1.25; color: #007bff; background-color: #fff; border: 1px solid #dee2e6;">{{ $page }}</a>
-                                        </li>
-                                    @endforeach
-                            
-                                    <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
-                                        <a class="page-link" href="{{ $data->nextPageUrl() }}" style="position: relative; display: block; padding: 0.5rem 0.75rem; margin-left: -1px; line-height: 1.25; color: #007bff; background-color: #fff; border: 1px solid #dee2e6;">Next</a>
-                                    </li>
-                                </ul>
-                            </div>
-        
-        
-        
-        
+        <div class="pagination-container" style="margin-top: 20px; display: flex; flex-direction: column; align-items: center;">
+                <div class="pagination-info mb-2" style="margin-bottom: 10px; color:white;">
+                    Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah Data
                 </div>
+                    <ul class="pagination" style="display: flex; padding-left: 0; list-style: none;">
+                        <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
+                            <a class="page-link" href="{{ $data->previousPageUrl() }}" style="position: relative; display: block; padding: 0.5rem 0.75rem; margin-left: -1px; line-height: 1.25; color: white; background-color: #fff; border: 1px solid #dee2e6; font-size:12px; border-radius: 20px 0px 0px 20px;"><i class="fas fa-arrow-left" style="margin-right:10px;"></i>Previous</a>
+                        </li>
+
+                        @foreach ($data->getUrlRange($data->currentPage() - 0, $data->currentPage() + 2) as $page => $url)
+                            <li class="page-item {{ $page == $data->currentPage() ? 'active' : '' }}" style="margin-right: 5px;">
+                                <a class="page-link" href="{{ $url }}" style="position: relative; display: block; padding: 0.5rem 0.75rem; margin-left: -1px; line-height: 1.25; color: white; background-color: #fff; border: 1px solid #dee2e6; font-size:12px; ">{{ $page }}</a>
+                            </li>
+                        @endforeach
+
+                        <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
+                            <a class="page-link" href="{{ $data->nextPageUrl() }}" style="position: relative; display: block; padding: 0.5rem 0.75rem; margin-left: -1px; line-height: 1.25; color: white; background-color: #fff; border: 1px solid #dee2e6; font-size:12px; border-radius: 0px 20px 20px 0px;">Next <i class="fas fa-arrow-right" style="margin-left:10px;"></i></a>
+                        </li>
+                    </ul>
+
+            </div>
 
 
                 
-            </div>
-            @include('backend.00_dashboard.part.menufooter')
         </div>
+    </div>
+    @include('backend.00_dashboard.part.menufooter')
         </div>
         </div>
         
