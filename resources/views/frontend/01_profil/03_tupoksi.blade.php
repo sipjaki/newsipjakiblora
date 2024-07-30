03413fe81448c0dfbb247fbd48ef42a49cd22ed8
Sigimatika <sigitechindo@gmail.com>
2024-07-30 17:49:31 +0700

    dfgdfg

--- resources/views/frontend/01_profil/03_tupoksi.blade.php
+++ resources/views/frontend/01_profil/03_tupoksi.blade.php
@@ -52,7 +52,7 @@ Tupoksi Seksi Program Dan Jasa Konstruksi
     border-radius: 25px;
     text-align: center;
     width: 100%;
-    height: 120vh;
+    height: 200vh;
     background: linear-gradient(to bottom, yellow, white, black);
     align-items: center;
     position: relative;
