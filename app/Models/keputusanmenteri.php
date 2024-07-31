488cdfb066e6bc70ff5617b6e0177bd32e26f27d
Sigimatika <sigitechindo@gmail.com>
2024-07-31 17:03:17 +0700

    d

--- app/Models/keputusanmenteri.php
+++ app/Models/keputusanmenteri.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class keputusanmenteri extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasApiTokens;
+
+    protected $guarded = ['id'];
 }
