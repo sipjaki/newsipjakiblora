26680b33c41fb8b41ef32f965a425b88e2c15b9d
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-08-22 11:49:24 +0700

    dfg

--- database/factories/SkktenagakerjaFactory.php
+++ database/factories/SkktenagakerjaFactory.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\skktenagakerja>
  */
-class skktenagakerjaFactory extends Factory
+class SkktenagakerjaFactory extends Factory
 {
     protected $model = skktenagakerja::class;
 
