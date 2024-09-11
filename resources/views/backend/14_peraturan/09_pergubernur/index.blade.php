7df89f15c4c72f8ebcc953dc365e568526fac4bc
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-09-11 14:06:29 +0700

    dfg

--- resources/views/backend/14_peraturan/09_pergubernur/index.blade.php
+++ resources/views/backend/14_peraturan/09_pergubernur/index.blade.php
@@ -31,7 +31,7 @@
     border-radius: 25px;
     text-align: center;
     width: 100%;
-    height: 150vh;
+    height: 160vh;
     margin-left: none;
     background: linear-gradient(to bottom, yellow, white, black);
     align-items: center;
