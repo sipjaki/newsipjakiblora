dd82d5abedff2be04ee0af7fd20d665803c42cb1
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-10-07 10:48:48 +0700

    d

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -86,7 +86,7 @@ class DatabaseSeeder extends Seeder
 
         
         asosiasipengusaha::factory(15)->create();
-        paketpekerjaan::factory(15)->create();
+        Paketpekerjaan::factory(15)->create();
         penanggungjawabteknis::factory(15)->create();
         ketertiban::factory(15)->create();
         beritaagenda::factory(15)->create();  
