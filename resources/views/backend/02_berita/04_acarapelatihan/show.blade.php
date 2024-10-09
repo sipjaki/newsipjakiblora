39403294a2106a9eb6cd2f1405764e5d2536e8a0
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-10-09 09:35:51 +0700

    d

--- resources/views/backend/02_berita/04_acarapelatihan/show.blade.php
+++ resources/views/backend/02_berita/04_acarapelatihan/show.blade.php
@@ -108,7 +108,7 @@
             </button>
         </a>
         <a href="/acarapelatihancreate" style="background: inherit">
-            <button class="badge-plus" style="border: none; font-size:15px; cursor:pointer; float:right; margin-left:440px; margin-right:5px;">
+            <button class="badge-plus" style="border: none; font-size:15px; cursor:pointer; float:right; >
                 <i class="fas fa-plus-circle mr-2"></i>Create New
             </button>
         </a>
