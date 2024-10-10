d0fec98c7381abf0427f074f511d20b716c1aba5
sigitseptiadi <sigitseptiadi1@gmail.com>
2024-10-10 13:25:59 +0700

    d

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '10e032c8403f6be14c2a3193fe0fbd6acd5874f3',
+        'reference' => '6f0b52857ac2ed027e0577f6af3bd5c594c8dd2b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -418,7 +418,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '10e032c8403f6be14c2a3193fe0fbd6acd5874f3',
+            'reference' => '6f0b52857ac2ed027e0577f6af3bd5c594c8dd2b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -802,8 +802,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
