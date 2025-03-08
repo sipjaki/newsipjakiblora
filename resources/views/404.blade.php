d3095985c06bafb9e5ed7f5d610ec9304e82ff0f
abgblora <abgblorabangunangedung@gmail.com>
2025-03-08 14:11:38 +0700

    f

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,3 +1,53 @@
+<style>
+        <style>
+      html, body {
+        height: 100%;
+      }
+      body {
+        background-color: #fff;
+        background: radial-gradient(circle at center, #fff 0%, #f8f8f8 75%, #ebebeb 100%);
+        color: #222;
+        font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif;
+        font-size: 1rem;
+        line-height: 1.5;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      main {
+        padding: 1rem;
+        text-align: center;
+      }
+      h1 {
+        font-size: 2.5rem;
+        line-height: 1.1;
+        margin: 0;
+      }
+      @media screen and (max-width: 480px) {
+        h1 {
+          font-size: 1.5rem;
+        }
+      }
+      h1::after {
+        content: "";
+        background-color: #ffe800;
+        background: repeating-linear-gradient(45deg, #ffe800, #ffe800 0.5rem, #222 0.5rem, #222 1.0rem);
+        display: block;
+        height: 0.5rem;
+        margin-top: 1rem;
+      }
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+      p {
+        margin: 1rem 0 0 0;
+      }
+    </style>
+
+</style>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -43,6 +93,11 @@
       <div class="w-full text-center my-auto">
         <h1 class="font-bold text-5xl font-Comforter-Brush text-amber-500 mb-10 mt-10 md:text-right">Coming Soon</h1>
 
+        <main>
+            <h1>Dinas Pekerjaan Umum dan Penataan Ruang <br> Kabupaten Blora Provinsi Jawa Tengah</h1>
+            <p>Our Website Is Under Construction <br> Please Wait</p>
+          </main>
+
         {{-- <h6 class="font-Montserrat font-bold uppercase text-4xl md:text-5xl lg:text-3xl mb-14 md:-mr-20 text-white md:text-left">Pemerintah <br> Provinsi Jawa Barat<span class="text-white/70 md:-mr-20"></span></h6> --}}
 
         <div>
@@ -86,3 +141,4 @@
 
 </body>
 </html>
+
