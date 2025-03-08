2cd9cd87af88ded06aed994889f49bd033583f6a
abgblora <abgblorabangunangedung@gmail.com>
2025-03-08 22:41:52 +0700

    g

--- resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
+++ resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
@@ -10,7 +10,7 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="/assets/abgblora/images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/abgblora/logo/logokabblora.png">
 
     <!-- CSS
 	============================================ -->
