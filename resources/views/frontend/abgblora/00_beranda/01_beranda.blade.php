b22acf32aad46d9d12ccc2308219f2f4c2ef3da8
abgblora <abgblorabangunangedung@gmail.com>
2025-03-08 23:34:04 +0700

    g

--- resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
+++ resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
@@ -1196,8 +1196,8 @@
                         <div class="col-lg-3 col-sm-6">
                             <!-- Footer Widget Start -->
                             <div class="footer-widget-about">
-                                <a class="footer-logo" href="#"><img src="/assets/abgblora/logo/logokabupatenblora.png" alt="Logo" width="400px;"></a>
-                                <a class="footer-logo" href="#"><img src="/assets/abgblora/logo/logopupr.png" width="400px;" alt="Logo"></a>
+                                <a class="footer-logo" href="#"><img src="/assets/abgblora/logo/logokabupatenblora.png" alt="Logo" width="200px;"></a>
+                                <a class="footer-logo" href="#"><img src="/assets/abgblora/logo/logopupr.png" width="200px;" alt="Logo"></a>
                                 <div class="widget-info">
                                     <ul>
                                         <li>
