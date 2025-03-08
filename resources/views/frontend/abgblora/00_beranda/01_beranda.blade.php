20079786218aeac3c4c9cb12b6c95bff81956399
abgblora <abgblorabangunangedung@gmail.com>
2025-03-08 23:51:35 +0700

    g

--- resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
+++ resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
@@ -1313,9 +1313,10 @@
                                 <!-- Footer Copyright Social Start -->
                                 <div class="copyright-social">
                                     <ul class="social">
+                                        <li><a href="mailto:youremail@example.com"><i class="fas fa-envelope"></i></a></li>
                                         <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-tiktok"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
                                     </ul>
                                 </div>
                                 <!-- Footer Copyright Social End -->
