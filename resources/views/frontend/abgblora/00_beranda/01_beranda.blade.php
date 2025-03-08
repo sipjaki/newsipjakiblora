daa9dd666f1521e51c86c225db84f9a505c52c09
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 00:25:26 +0700

    g

--- resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
+++ resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
@@ -1224,13 +1224,13 @@
                                         <img src="/assets/abgblora/logo/logopupr.png" alt="Logo" width="75">
                                     </a>
                                 </div>
-                                <h2 style="display: flex; align-items: center; gap: 5px; margin: 0;">
+                                <h4 style="display: flex; align-items: center; gap: 5px; margin: 0;">
                                     <span style="color: white; font-style: italic;">ABG</span>
                                     <span style="color: green; font-style: italic;">BLORA</span>
-                                </h2>
-                                <h2>
+                                </h4>
+                                <h4>
                                     <span style="color: #FFD100; font-style: italic;">BANGUNAN GEDUNG</span>
-                                </h2>
+                                </h4>
                                       {{-- <div class="widget-info">
                                         <ul>
                                         <li>
