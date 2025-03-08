7499daf3113510a75e529b59447c6d10f5bfa322
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 00:08:50 +0700

    g

--- resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
+++ resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
@@ -219,7 +219,8 @@
             <div class="offcanvas-header">
                 <!-- Offcanvas Logo Start -->
                 <div class="offcanvas-logo">
-                    <a href="index.html"><img src="/assets/abgblora/logo/logokabupatenblora.png" alt=""></a>
+                    <a href="index.html"><img src="/assets/abgblora/logo/logokabupatenblora.png" alt="" width="15"></a>
+                    <a href="index.html"><img src="/assets/abgblora/logo/logopupr.png" alt="" width="15"></a>
                 </div>
                 <!-- Offcanvas Logo End -->
                 <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
