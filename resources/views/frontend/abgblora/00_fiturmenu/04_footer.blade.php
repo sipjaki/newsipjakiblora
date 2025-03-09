859e219827d3b3492487e44a4aebbdec3aa123a8
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 14:24:40 +0700

    g

---
+++ resources/views/frontend/abgblora/00_fiturmenu/04_footer.blade.php
@@ -0,0 +1,29 @@
+
+    <!-- JS
+    ============================================ -->
+    <script src="/assets/abgblora/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="/assets/abgblora/js/vendor/modernizr-3.11.2.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="/assets/abgblora/js/plugins/popper.min.js"></script>
+    <script src="/assets/abgblora/js/plugins/bootstrap.min.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="/assets/abgblora/js/plugins/swiper-bundle.min.js"></script>
+    <script src="/assets/abgblora/js/plugins/aos.js"></script>
+    <script src="/assets/abgblora/js/plugins/waypoints.min.js"></script>
+    <script src="/assets/abgblora/js/plugins/back-to-top.js"></script>
+    <script src="/assets/abgblora/js/plugins/jquery.counterup.min.js"></script>
+    <script src="/assets/abgblora/js/plugins/appear.min.js"></script>
+    <script src="/assets/abgblora/js/plugins/jquery.magnific-popup.min.js"></script>
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+
+    <!-- Main JS -->
+    <script src="/assets/abgblora/js/main.js"></script>
+
+</body>
+
+</html>
