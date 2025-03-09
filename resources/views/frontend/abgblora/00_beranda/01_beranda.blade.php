86f17f994f12f933862decf736091621c7346f3b
abgblora <abgblorabangunangedung@gmail.com>
2025-03-09 13:52:38 +0700

    g

--- resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
+++ resources/views/frontend/abgblora/00_beranda/01_beranda.blade.php
@@ -229,68 +229,7 @@
 
         {{-- JANGAN DI HAPUS  ====================================================--}}
 
-        <!-- Offcanvas Start-->
-        <div class="offcanvas offcanvas-start" id="offcanvasExample">
-            <div class="offcanvas-header">
-                <!-- Offcanvas Logo Start -->
-                <div style="display: flex; align-items: center; gap: 10px;">
-                    <a href="index.html">
-                        <img src="/assets/abgblora/logo/logokabupatenblora.png" alt="Logo Kabupaten Blora" style="width: 50px; height: auto;">
-                    </a>
-                    <a href="index.html">
-                        <img src="/assets/abgblora/logo/logopupr.png" alt="Logo PUPR" style="width: 50px; height: auto;">
-                    </a>
-                </div>
-                <!-- Offcanvas Logo End -->
-                <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
-            </div>
-
-            <!-- Offcanvas Body Start -->
-            <div class="offcanvas-body">
-                <div class="offcanvas-menu">
-                    <ul class="main-menu">
-                        <li class="active-menu">
-                            <a href="/404">Persetujuan Bangunan Gedung SLF</a>
-                            {{-- <ul class="sub-menu"> --}}
-                                {{-- <li class="active"><a href="index.html">Home One</a></li>
-                                <li><a href="index-2.html">Home Two</a></li>
-                                <li><a href="index-3.html">Home Three</a></li>
-                                <li><a href="index-4.html">Home Four</a></li>
-                                <li><a href="index-5.html">Home Five</a></li>
-                                <li><a href="index-6.html">Home Six</a></li> --}}
-                            {{-- </ul> --}}
-                        </li>
-                        <li>
-                            <a href="/404">Tracking PBG</a>
-                        </li>
-                        <li><a href="/404">Pendataan Bangunan Gedung</a>
-                            {{-- <ul class="sub-menu">
-                                <li><a href="team.html">Our Team</a></li>
-                                <li><a href="service.html">Service</a></li>
-                                <li><a href="why-choose.html">Why Choose Us</a></li>
-                                <li><a href="testimonial.html">Testimonial</a></li>
-                                <li><a href="pricing.html">Pricing</a></li>
-                                <li><a href="login-register.html">Login & Register</a></li>
-                            </ul> --}}
-                        </li>
-                        <li><a href="/404">Bantuan Teknis</a>
-                            {{-- <ul class="sub-menu">
-                                <li><a href="blog.html">Blog Grid</a></li>
-                                <li><a href="blog-standard.html">Latest News</a></li>
-                                <li><a href="blog-details.html">Blog Details</a></li>
-                            </ul> --}}
-                        </li>
-                        <li><a href="/404">Sosialisasi & Pelatihan</a></li>
-                        <li><a href="/404">Keterangan Rencana Kota (KRK) </a></li>
-                        <li><a href="/404">Penilik Bangunan Gedung</a></li>
-                        <li><a href="/404">SPPD</a></li>
-                    </ul>
-                </div>
-            </div>
-            <!-- Offcanvas Body End -->
-        </div>
-        <!-- Offcanvas End -->
-
+        @include('frontend.abgblora.00_fiturmenu.01_canvasmenuhp')
 
         <!-- Service Start -->
         <div class="section service-section" style="margin-top:350px;">
