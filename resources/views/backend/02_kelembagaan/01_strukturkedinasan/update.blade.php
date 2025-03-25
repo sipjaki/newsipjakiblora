ad93a883261de6d9fb8543e364fd6b5f52c85f85
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-26 00:57:32 +0700

    g

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
@@ -105,7 +105,30 @@
                                 </div>
                                 <!-- end::Body -->
 
-                                <button type="submit" class="btn btn-primary">Update</button>
+                                <br><br>
+                                <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
+
+                                    <button type="submit"
+                                        onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                                        onmouseout="this.style.backgroundColor='#22C55E'; this.style.color='white';"
+                                        style="background-color: #117235; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                                        <!-- Ikon Kembali -->
+                                        <i class="fa fa-file" style="margin-right: 8px;"></i>
+                                        Update
+                                    </button>
+
+                                    <a href="/beagendapelatihan">
+                                        <button
+                                        onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                                        onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
+                                        style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                                        <!-- Ikon Kembali -->
+                                        <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
+                                        Kembali
+                                    </button>
+                                </a>
+                            </div>
+
                             </form>
 
                         </div>
@@ -114,29 +137,6 @@
                     </div>
                     <!-- /.card -->
                     <!-- Button Section -->
-                    <br><br>
-                    <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
-                        <a href="/404">
-                            <button
-                            onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                            onmouseout="this.style.backgroundColor='#22C55E'; this.style.color='white';"
-                            style="background-color: #22C55E; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                            <!-- Ikon Kembali -->
-                            <i class="fa fa-file" style="margin-right: 8px;"></i>
-                            Update
-                        </button>
-                        </a>
-                        <a href="/beagendapelatihan">
-                            <button
-                            onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                            onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
-                            style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                            <!-- Ikon Kembali -->
-                            <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
-                            Kembali
-                        </button>
-                    </a>
-                </div>
 
                     </div>
                     <!--end::Row-->
