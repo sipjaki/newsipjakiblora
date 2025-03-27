b28d081917c1adb238909ae62ba70faa40579b3e
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-28 06:37:05 +0700

    df

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
@@ -126,17 +126,21 @@
                                             <input type="file" name="peraturan" class="form-control @error('peraturan') is-invalid @enderror" id="peraturan" onchange="previewImage()" />
 
                                             <div style="margin-top: 10px;">
-                                                @if($data->peraturan)  <!-- Cek jika ada file sebelumnya -->
-                                                    <object data="{{ asset('storage/' . $data->peraturan) }}" type="application/pdf" width="300" height="200">
+                                                @if($strukturdinas->peraturan)  <!-- Cek jika ada file setelah diupload -->
+                                                    <object data="{{ asset('storage/' . $strukturdinas->peraturan) }}" type="application/pdf" width="300" height="200">
                                                         <p>PDF cannot be displayed.</p>
                                                     </object>
+                                                @else
+                                                    <p>No file uploaded yet.</p>
                                                 @endif
+
                                                 @error('peraturan')
                                                     <div class="invalid-feedback">
                                                         {{$message}}
                                                     </div>
                                                 @enderror
                                             </div>
+
                                         </div>
 
                                         <script>
