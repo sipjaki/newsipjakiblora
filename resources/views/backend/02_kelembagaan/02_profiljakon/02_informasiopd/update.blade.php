0689390d8f817983215dbd797711b0d136134aa3
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-28 20:24:27 +0700

    fd

--- resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/update.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/update.blade.php
@@ -88,7 +88,7 @@
         {{-- ======================================================= --}}
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
-                        <form action="{{ route('update.beinformasiopdupdatecreate', $data->id) }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('update.beinfoopdupdatecreate', $data->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('POST') <!-- Ganti dengan PUT jika Anda ingin menggunakan metode PUT untuk update -->
 
