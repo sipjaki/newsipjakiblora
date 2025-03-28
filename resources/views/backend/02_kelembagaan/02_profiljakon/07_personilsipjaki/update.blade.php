4a7db317cadfdb8dd2a145ec313b968546899680
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 03:54:55 +0700

    d

--- resources/views/backend/02_kelembagaan/02_profiljakon/07_personilsipjaki/update.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/07_personilsipjaki/update.blade.php
@@ -88,7 +88,7 @@
         {{-- ======================================================= --}}
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
-                        <form action="{{ route('update.besubbidcreateupdate', $data->id) }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('update.besipjakicreateupdate', $data->id) }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             @method('POST') <!-- Ganti dengan PUT untuk update -->
 
