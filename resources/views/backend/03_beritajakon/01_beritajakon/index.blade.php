7bd36ae945c9cbd3b1b0b2688b398ac4782bceb8
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 06:03:43 +0700

    df

--- resources/views/backend/03_beritajakon/01_beritajakon/index.blade.php
+++ resources/views/backend/03_beritajakon/01_beritajakon/index.blade.php
@@ -107,20 +107,19 @@
 
                                 </script>
 
-                            </div>
-                    <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
-                        <a href="/betupoksi/create">
-                            <button
-                            onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                            onmouseout="this.style.backgroundColor='#156f2a'; this.style.color='white';"
-                            style="background-color: #156f2a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                            <!-- Ikon Kembali -->
-                            <i class="fa fa-plus" style="margin-right: 8px;"></i>
-                            Create
-                        </button>
-                        </a>
 
-                    </div>
+                            <a href="/betupoksi/create">
+                                <button
+                                onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                                onmouseout="this.style.backgroundColor='#156f2a'; this.style.color='white';"
+                                style="background-color: #156f2a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                                <!-- Ikon Kembali -->
+                                <i class="fa fa-plus" style="margin-right: 8px;"></i>
+                                Create
+                            </button>
+                            </a>
+
+                            </div>
                     <hr>
                     <!-- /.card-header -->
                     <div class="card-body p-0">
