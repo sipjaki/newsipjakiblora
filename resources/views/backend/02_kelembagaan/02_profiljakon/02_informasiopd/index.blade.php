eb5e4d62844261b88c7692af166a22ae4cb984c9
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-28 19:35:13 +0700

    g

--- resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/index.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/index.blade.php
@@ -176,8 +176,8 @@
                         <a href="/beinfoopd/update/{{$item->namaopd}}">
                             <button
                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                            onmouseout="this.style.backgroundColor='#156f2a'; this.style.color='white';"
-                            style="background-color: #156f2a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            onmouseout="this.style.backgroundColor='#117235'; this.style.color='white';"
+                            style="background-color: #117235; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                             <!-- Ikon Kembali -->
                             <i class="fa fa-file" style="margin-right: 8px;"></i>
                             Update
@@ -186,8 +186,8 @@
                         <a href="/beprofiljakon">
                             <button
                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                            onmouseout="this.style.backgroundColor='navy'; this.style.color='white';"
-                            style="background-color: navy; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
+                            style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                             <!-- Ikon Kembali -->
                             <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
                             Kembali
