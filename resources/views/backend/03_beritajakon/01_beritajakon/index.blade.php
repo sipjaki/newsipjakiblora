f382fbc39cdc1b32dc0cf7674dfd30a851b181be
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 13:07:44 +0700

    df

--- resources/views/backend/03_beritajakon/01_beritajakon/index.blade.php
+++ resources/views/backend/03_beritajakon/01_beritajakon/index.blade.php
@@ -77,19 +77,18 @@
                     ⚙️ Setting Database
                 </div>
 
-                     <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
-                         <a href="/404">
-                             <button
-                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                             onmouseout="this.style.backgroundColor='#166534'; this.style.color='white';"
-                             style="background-color: #166534; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                             <!-- Ikon Kembali -->
-                             <i class="fa fa-database" style="margin-right: 8px;"></i>
-                             Asosiasi
-                         </button>
-                         </a>
+                <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
+                    <a href="/404">
+                        <button
+                            onmouseover="this.style.backgroundColor='#1F3B2D'; this.style.color='#FFFFFF';"
+                            onmouseout="this.style.backgroundColor='#1F3B2D'; this.style.color='#FFFFFF';"
+                            style="background-color: #1F3B2D; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            <i class="fa fa-database" style="margin-right: 8px;"></i>
+                            Asosiasi
+                        </button>
+                    </a>
+                </div>
 
-                     </div>
                  </div>
                  <!-- /.card-header -->
                  <div class="card-header">
@@ -139,21 +138,11 @@
                                 </script>
 
 
-                        <a href="/bebujkjakon">
+                         <a href="/beberitajakon/create">
                              <button
                              onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                             onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
-                             style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                             <!-- Ikon Kembali -->
-                             <i class="fa fa-arrow-left" style="margin-right: 8px;"></i> Kembali
-
-                         </button>
-                         </a>
-                         <a href="/404">
-                             <button
-                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                             onmouseout="this.style.backgroundColor='#166534'; this.style.color='white';"
-                             style="background-color: #166534; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                             onmouseout="this.style.backgroundColor='#007bff'; this.style.color='white';"
+                             style="background-color: #007bff; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                              <!-- Ikon Kembali -->
                              <i class="fa fa-plus" style="margin-right: 8px;"></i>
                              Create
