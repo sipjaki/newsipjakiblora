5bf5d9fac85f526324c563c1df350d9d4bebe352
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 13:10:50 +0700

    g

--- resources/views/backend/03_beritajakon/01_beritajakon/create.blade.php
+++ resources/views/backend/03_beritajakon/01_beritajakon/create.blade.php
@@ -206,7 +206,7 @@
 
                             <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
                                 <button type="submit" style="background-color: #007bff; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
-                                    <i class="fa fa-file" style="margin-right: 8px;"></i> Create
+                                    <i class="fa fa-pencil" style="margin-right: 8px;"></i> Create
                                 </button>
                             </div>
                         </form>
