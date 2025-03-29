3af9075191da245a7e65205a87112cf08575d150
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 14:31:28 +0700

    df

---
+++ resources/views/backend/00_administrator/00_baganterpisah/08_style.blade.php
@@ -0,0 +1,12 @@
+<style>
+    /* Mengatur font Poppins untuk seluruh halaman */
+body {
+    font-family: 'Poppins', sans-serif;
+}
+
+/* Mengatur font Poppins untuk elemen lainnya jika diperlukan */
+h1, h2, h3, h4, h5, h6, p, a, span, div {
+    font-family: 'Poppins', sans-serif;
+}
+
+</style>
