62f3cc083a87276061aba4cf36d6c1bc16dc80a8
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 15:14:35 +0700

    er

--- resources/views/backend/03_beritajakon/02_artikeljakon/create.blade.php
+++ resources/views/backend/03_beritajakon/02_artikeljakon/create.blade.php
@@ -89,7 +89,7 @@
         {{-- ======================================================= --}}
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
-                        <form action="{{ route('create.beberitajakoncreatenew') }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('create.beartikeljakoncreatenew') }}" method="POST" enctype="multipart/form-data">
                             @csrf
                             <!-- begin::Body -->
                             <div class="card-body">
