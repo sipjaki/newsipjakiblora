b3167851adeda43aa5220502366d5fbd9268e402
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 15:27:58 +0700

    re

--- resources/views/backend/03_beritajakon/02_artikeljakon/create.blade.php
+++ resources/views/backend/03_beritajakon/02_artikeljakon/create.blade.php
@@ -72,7 +72,7 @@
 
         <div class="card card-primary card-outline mb-6">
             <div style="display: flex; justify-content: flex-end; margin-top:10px;">
-                <a href="/beberitajakon">
+                <a href="/beartikeljakon">
                     <button
                     onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                     onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
