120b6699d17f00de3f152294f7ee6f34f6e729f2
SipjakiBlora <sipjakiblora@gmail.com>
2025-03-29 07:50:05 +0700

    g

--- resources/views/backend/03_beritajakon/01_beritajakon/create.blade.php
+++ resources/views/backend/03_beritajakon/01_beritajakon/create.blade.php
@@ -205,7 +205,7 @@
                             <!-- end::Body -->
 
                             <div style="display: flex; justify-content: flex-end; margin-bottom: 20px;">
-                                <button type="submit" style="background-color: #117235; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
+                                <button type="submit" style="background-color: #007bff; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
                                     <i class="fa fa-file" style="margin-right: 8px;"></i> Create
                                 </button>
                             </div>
