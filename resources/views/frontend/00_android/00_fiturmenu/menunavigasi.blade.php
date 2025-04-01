16c391233f235f71550307ae58f9184d6c580b5a
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 22:12:28 +0700

    g

--- resources/views/frontend/00_android/00_fiturmenu/menunavigasi.blade.php
+++ resources/views/frontend/00_android/00_fiturmenu/menunavigasi.blade.php
@@ -1,12 +1,20 @@
 
 <div id="Feature" class="px-[18px] relative z-10">
     <div class="bg-white p-[18px_16px] rounded-xl overflow-hidden grid grid-cols-4 gap-[27px]">
+      <a href="/">
+        <div class="flex flex-col items-center gap-2">
+          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
+            <img src="/assets/00_android/iconmenu/beranda.png" class="w-full h-full object-cover" alt="icon">
+          </div>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Beranda</p>
+        </div>
+      </a>
       <a href="/404">
         <div class="flex flex-col items-center gap-2">
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/1.berita.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">PBG/SLF</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Berita</p>
         </div>
       </a>
       <a href="/404">
@@ -14,7 +22,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/2.kelembagaan.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Tracking</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Kelembagaan</p>
         </div>
       </a>
       <a href="/404">
@@ -22,7 +30,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/3.datajakon.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pendataan</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Data Jakon</p>
         </div>
       </a>
       <a href="/404">
@@ -30,7 +38,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/4.pembinaan.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Bantek</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pembinaan</p>
         </div>
       </a>
       <a href="/404">
@@ -38,7 +46,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/5.pengawasan.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Sosialisasi</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pengawasan</p>
         </div>
       </a>
       <a href="/404">
@@ -46,7 +54,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/6.ahsp.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">KRK</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">AHSP</p>
         </div>
       </a>
       <a href="/404">
@@ -54,7 +62,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/00_android/iconmenu/7.rantaipasok.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Penilik</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Rantai Pasok</p>
         </div>
       </a>
       <a href="/web">
@@ -62,7 +70,7 @@
           <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
             <img src="/assets/android/iconmenu/08.png" class="w-full h-full object-cover" alt="icon">
           </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">WEB</p>
+          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Web</p>
         </div>
       </a>
       {{-- <a href="/404">
