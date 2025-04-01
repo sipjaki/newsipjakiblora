5ecd445e4738b32f2a336b61326b32cf87e29658
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 19:36:10 +0700

    g

---
+++ resources/views/frontend/00_android/00_fiturmenu/header.blade.php
@@ -0,0 +1,12 @@
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/assets/00_android/output.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="/assets/00_android/js/flickity.min.css" media="screen">
+</head>
+<body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
