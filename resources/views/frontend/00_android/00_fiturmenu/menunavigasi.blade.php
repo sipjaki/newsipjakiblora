810ac5157a1d87a37eac9f2fda11879e766c8d05
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 22:16:25 +0700

    g

--- resources/views/frontend/00_android/00_fiturmenu/menunavigasi.blade.php
+++ resources/views/frontend/00_android/00_fiturmenu/menunavigasi.blade.php
@@ -1,75 +1,93 @@
 <div id="Feature" class="px-[18px] relative z-10">
     <div class="bg-white p-[18px_16px] rounded-xl overflow-hidden grid grid-cols-4 gap-[27px]">
         <a href="/">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/beranda.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Beranda</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Beranda
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/1.berita.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Berita</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Berita
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/2.kelembagaan.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Kelembagaan</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Kelembagaan
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/3.datajakon.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Data Jakon</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Data Jakon
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/4.pembinaan.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pembinaan</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Pembinaan
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/5.pengawasan.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pengawasan</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Pengawasan
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/6.ahsp.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">AHSP</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    AHSP
+                </p>
             </div>
         </a>
         <a href="/404">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/00_android/iconmenu/7.rantaipasok.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Rantai Pasok</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Rantai Pasok
+                </p>
             </div>
         </a>
         <a href="/web">
-            <div class="flex flex-col items-center gap-2">
+            <div class="flex flex-col items-center gap-2 w-full">
                 <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
                     <img src="/assets/android/iconmenu/08.png" class="w-[80%] h-[80%] object-contain" alt="icon">
                 </div>
-                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Web</p>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px] w-full text-center whitespace-nowrap">
+                    Web
+                </p>
             </div>
         </a>
     </div>
