88ef14002a1706c4b2e77207b85cd173627f66f9
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-02 01:37:33 +0700

    g

--- resources/views/frontend/00_android/02_satuanhargadasar/index.blade.php
+++ resources/views/frontend/00_android/02_satuanhargadasar/index.blade.php
@@ -44,6 +44,34 @@
                             </div>
                             </a>
 
+                        <a href="/" class="card">
+                            <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
+                                <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
+                                    <img src="/assets/icon/pupr.png" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
+                                </div>
+
+                            <div class="flex flex-col gap-1">
+                        <p class="font-bold line-clamp-1 hover:line-clamp-none" style="color: black;">Satuan Harga Upah Tenaga Kerja</p>
+                        </p>
+
+                            </div>
+                            </div>
+                            </a>
+
+                            <a href="/" class="card">
+                            <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
+                                <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
+                                    <img src="/assets/icon/pupr.png" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
+                                </div>
+
+                            <div class="flex flex-col gap-1">
+                        <p class="font-bold line-clamp-1 hover:line-clamp-none" style="color: black;">Satuan Harga Peralatan</p>
+                        </p>
+
+                            </div>
+                            </div>
+                            </a>
+
                         </div>
 
                         @include('frontend.00_android.00_fiturmenu.keterangan')
