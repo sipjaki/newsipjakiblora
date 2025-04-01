b8d3c69a27b56371e173f95f2634fea52ca30cfa
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 22:13:55 +0700

    g

--- resources/views/frontend/00_android/00_fiturmenu/menunavigasi.blade.php
+++ resources/views/frontend/00_android/00_fiturmenu/menunavigasi.blade.php
@@ -1,103 +1,76 @@
-
 <div id="Feature" class="px-[18px] relative z-10">
     <div class="bg-white p-[18px_16px] rounded-xl overflow-hidden grid grid-cols-4 gap-[27px]">
-      <a href="/">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/beranda.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Beranda</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/1.berita.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Berita</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/2.kelembagaan.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Kelembagaan</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/3.datajakon.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Data Jakon</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/4.pembinaan.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pembinaan</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/5.pengawasan.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pengawasan</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/6.ahsp.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">AHSP</p>
-        </div>
-      </a>
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/00_android/iconmenu/7.rantaipasok.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Rantai Pasok</p>
-        </div>
-      </a>
-      <a href="/web">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/android/iconmenu/08.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Web</p>
-        </div>
-      </a>
-      {{-- <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/android/iconmenu/09.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Bantuan</p>
-        </div>
-      </a>
-
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/android/iconmenu/10.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Hibah</p>
-        </div>
-      </a>
-
-      <a href="/404">
-        <div class="flex flex-col items-center gap-2">
-          <div class="w-[60px] h-[60px] flex shrink-0 rounded-full overflow-hidden">
-            <img src="/assets/android/iconmenu/webversion.png" class="w-full h-full object-cover" alt="icon">
-          </div>
-          <p class="font-medium text-sm text-[#757C98] leading-[21px]">Web</p>
-        </div>
-      </a> --}}
+        <a href="/">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/beranda.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Beranda</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/1.berita.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Berita</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/2.kelembagaan.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Kelembagaan</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/3.datajakon.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Data Jakon</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/4.pembinaan.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pembinaan</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/5.pengawasan.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Pengawasan</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/6.ahsp.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">AHSP</p>
+            </div>
+        </a>
+        <a href="/404">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/00_android/iconmenu/7.rantaipasok.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Rantai Pasok</p>
+            </div>
+        </a>
+        <a href="/web">
+            <div class="flex flex-col items-center gap-2">
+                <div class="w-[50px] h-[50px] flex justify-center items-center shrink-0 rounded-full bg-gray-100 overflow-hidden">
+                    <img src="/assets/android/iconmenu/08.png" class="w-[80%] h-[80%] object-contain" alt="icon">
+                </div>
+                <p class="font-medium text-sm text-[#757C98] leading-[21px]">Web</p>
+            </div>
+        </a>
     </div>
-  </div>
+</div>
