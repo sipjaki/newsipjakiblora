435cf42cb985d40ddd760d03df855d6540f80120
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 22:45:53 +0700

    g

--- resources/views/frontend/00_android/00_halamanutama/index.blade.php
+++ resources/views/frontend/00_android/00_halamanutama/index.blade.php
@@ -129,7 +129,7 @@
                             </div>
                         </a>
                         <div class="flex flex-col gap-[6px]">
-                            <a href="/404" class="font-bold line-clamp-2 hover:line-clamp-none">Satuan Harga Dasar</a>
+                            <a href="/404" class="font-bold line-clamp-2 hover:line-clamp-none text-sm">Satuan Harga Dasar</a>
                             {{-- <p class="text-xs leading-[18px]">Target <span class="font-bold text-[#FF7815]">Rp 800.000.000</span></p> --}}
                         </div>
                         {{-- <progress id="fund" value="32" max="100" class="w-full h-[6px] rounded-full overflow-hidden"></progress> --}}
