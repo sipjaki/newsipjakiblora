4d028b5d6eb871d9b801bb44b9022442b257c3aa
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 20:01:36 +0700

    gf

--- resources/views/frontend/00_android/00_halamanutama/index.blade.php
+++ resources/views/frontend/00_android/00_halamanutama/index.blade.php
@@ -236,12 +236,7 @@
         </div>
 
         <div id="menu" class="max-w-[341px] w-full fixed bottom-[20px] p-3 flex items-center justify-between rounded-[30px] bg-[#1E2037] transform -translate-x-1/2 left-1/2">
-            <a href="" class="p-[14px_16px] flex items-center gap-[6px] rounded-full bg-[#FF7815]">
-                <div class="flex shrink-0">
-                    <img src="/assets/00_android/images/icons/heart.svg" alt="icon">
-                </div>
-                <span class="font-semibold text-sm text-white">Discover</span>
-            </a>
+
             <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
                 <div class="flex shrink-0 w-6 h-6 overflow-hidden">
                     <img src="/assets/00_android/images/icons/crown.svg" alt="icon">
@@ -250,6 +245,17 @@
             <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
                 <div class="flex shrink-0 w-6 h-6 overflow-hidden">
                     <img src="/assets/00_android/images/icons/3dcube.svg" alt="icon">
+                </div>
+            </a>
+            <a href="" class="p-[14px_16px] flex items-center gap-[6px] rounded-full bg-[#FF7815]">
+                <div class="flex shrink-0">
+                    <img src="/assets/00_android/images/icons/heart.svg" alt="icon">
+                </div>
+                {{-- <span class="font-semibold text-sm text-white">Discover</span> --}}
+            </a>
+            <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
+                <div class="flex shrink-0 w-6 h-6 overflow-hidden">
+                    <img src="/assets/00_android/images/icons/setting-2.svg" alt="icon">
                 </div>
             </a>
             <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
