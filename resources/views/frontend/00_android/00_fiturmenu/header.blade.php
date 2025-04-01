cdc4350d4157e3af18db66ac4f8132780793a4d4
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-01 20:07:50 +0700

    ge

--- resources/views/frontend/00_android/00_fiturmenu/header.blade.php
+++ resources/views/frontend/00_android/00_fiturmenu/header.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/assets/00_android/output.css" rel="stylesheet">
+    <link href="/assets/00_android/output/output.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <!-- CSS -->
     <link rel="stylesheet" href="/assets/00_android/js/flickity.min.css" media="screen">
--- resources/views/frontend/00_android/00_halamanutama/index.blade.php
+++ resources/views/frontend/00_android/00_halamanutama/index.blade.php
@@ -235,36 +235,8 @@
             </div>
         </div>
 
-        <div id="menu" class="max-w-[350px] w-full fixed bottom-[15px] p-3 flex items-center justify-between rounded-[30px] bg-[#1E2037] transform -translate-x-1/2 left-1/2">
 
-            <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
-                <div class="flex shrink-0 w-6 h-6 overflow-hidden">
-                    <img src="/assets/00_android/images/icons/crown.svg" alt="icon">
-                </div>
-            </a>
-            <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
-                <div class="flex shrink-0 w-6 h-6 overflow-hidden">
-                    <img src="/assets/00_android/images/icons/3dcube.svg" alt="icon">
-                </div>
-            </a>
-            <a href="" class="p-[14px_16px] flex items-center gap-[6px] rounded-full bg-[#FF7815]">
-                <div class="flex shrink-0">
-                    <img src="/assets/00_android/images/icons/heart.svg" alt="icon">
-                </div>
-                {{-- <span class="font-semibold text-sm text-white">Discover</span> --}}
-            </a>
-            <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
-                <div class="flex shrink-0 w-6 h-6 overflow-hidden">
-                    <img src="/assets/00_android/images/icons/setting-2.svg" alt="icon">
-                </div>
-            </a>
-            <a href="" class="flex items-center justify-center w-[56px] h-[52px] p-[14px_16px]">
-                <div class="flex shrink-0 w-6 h-6 overflow-hidden">
-                    <img src="/assets/00_android/images/icons/setting-2.svg" alt="icon">
-                </div>
-            </a>
-        </div>
-
+        @include('frontend.00_android.00_fiturmenu.android')
 
     </section>
 
