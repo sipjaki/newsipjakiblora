065e012ec4d2c039921cf3ede8b9a88b5312756d
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-02 13:28:36 +0700

    we

--- resources/views/frontend/00_android/02_satuanhargadasar/01_satuanhargamaterial.blade.php
+++ resources/views/frontend/00_android/02_satuanhargadasar/01_satuanhargamaterial.blade.php
@@ -126,17 +126,16 @@
                                     Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
                                 </div>
                             </div>
-
                             <!-- Pagination Section -->
                             <div class="pagination-container" style="margin-top: 50px; display: flex; flex-direction: column; align-items: center;">
                                 <ul class="pagination-paginate" style="display: flex; padding-left: 0; list-style: none; margin-top: 10px;">
                                     <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
-                                        <a class="page-link" href="{{ $data->previousPageUrl() }}">
+                                        <a class="page-link" href="{{ $data->previousPageUrl() }}" style="padding: 10px 20px; border: 1px solid #ccc; border-radius: 5px; text-decoration: none; color: black; font-size: 14px;">
                                             <i class="fas fa-arrow-left" style="margin-right: 10px;"></i>Previous
                                         </a>
                                     </li>
                                     <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
-                                        <a class="page-link" href="{{ $data->nextPageUrl() }}">
+                                        <a class="page-link" href="{{ $data->nextPageUrl() }}" style="padding: 10px 20px; border: 1px solid #ccc; border-radius: 5px; text-decoration: none; color: black; font-size: 14px;">
                                             Next <i class="fas fa-arrow-right" style="margin-left: 10px;"></i>
                                         </a>
                                     </li>
