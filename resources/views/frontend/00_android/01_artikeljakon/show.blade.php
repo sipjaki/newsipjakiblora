07d0860a7318ea4c398cbb8b77b64d6a674bd18e
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-02 20:29:38 +0700

    re

--- resources/views/frontend/00_android/01_artikeljakon/show.blade.php
+++ resources/views/frontend/00_android/01_artikeljakon/show.blade.php
@@ -66,9 +66,9 @@
                     <p class="desc-less text-sm leading-[26px]" style="text-align: justify;">{!!$data->keterangan!!}</p>
 
                 </div>
-                <div style="display: inline-block;">
+                <div style="display: inline-block; float: right;">
                     <button id="sertifikat-btn" class="badge"
-                       style="background-color: navy; color: white; border: none; transition: 0.3s; padding:15px 20px; font-size: 16px; border-radius:5px;"
+                       style="background-color: navy; color: white; border: none; transition: 0.3s; padding:10px 20px; font-size: 13px; border-radius:5px;"
                        onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.style.border='1px solid black';"
                        onmouseout="this.style.backgroundColor='navy'; this.style.color='white'; this.style.border='none';">
                         <i class="fas fa-download" style="margin-right:5px;"></i> Download .pdf
