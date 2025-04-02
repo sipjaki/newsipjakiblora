03655c2543e2640808969c510a6d98642003da00
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-02 13:53:16 +0700

    er

--- resources/views/frontend/00_android/02_satuanhargadasar/01_satuanhargamaterial.blade.php
+++ resources/views/frontend/00_android/02_satuanhargadasar/01_satuanhargamaterial.blade.php
@@ -52,7 +52,7 @@
                                     <button
                                     onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                                     onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
-                                    style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                                    style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 8px 10px; border-radius: 15px; font-size: 14px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                                     <!-- Ikon Kembali -->
                                     <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
                                     Kembali
