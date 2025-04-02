587859fc2dcd54abb6bb82524e9c2fc367480193
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-02 20:25:01 +0700

    g

--- resources/views/frontend/00_android/01_artikeljakon/index.blade.php
+++ resources/views/frontend/00_android/01_artikeljakon/index.blade.php
@@ -72,6 +72,28 @@
                             </a>
                             @endforeach
 
+                            <div class="pagination-info-box" style="margin: 20px 0; padding: 10px; border: 1px solid black; background-color: #f9f9f9; border-radius: 5px; width: 100%; text-align: center;">
+                                <div class="pagination-info" style="color: black; font-weight: 500; font-size: 14px; display: inline-block;">
+                                    Data Ke {{ $data->firstItem() }} Sampai {{ $data->lastItem() }} Dari {{ $data->total() }} Jumlah {{$title}}
+                                </div>
+                            </div>
+                            <!-- Pagination Section -->
+                            <div class="pagination-container" style="margin-top: 50px; display: flex; flex-direction: column; align-items: center;">
+                                <ul class="pagination-paginate" style="display: flex; padding-left: 0; list-style: none; margin-top: 10px;">
+                                    <li class="page-item {{ $data->onFirstPage() ? 'disabled' : '' }}" style="margin-right: 5px;">
+                                        <a class="page-link" href="{{ $data->previousPageUrl() }}" style="padding: 10px 20px; border: 1px solid #ccc; border-radius: 5px; text-decoration: none; color: black; font-size: 14px;">
+                                            <i class="fas fa-arrow-left" style="margin-right: 10px;"></i>Previous
+                                        </a>
+                                    </li>
+                                    <li class="page-item {{ $data->hasMorePages() ? '' : 'disabled' }}" style="margin-right: 5px;">
+                                        <a class="page-link" href="{{ $data->nextPageUrl() }}" style="padding: 10px 20px; border: 1px solid #ccc; border-radius: 5px; text-decoration: none; color: black; font-size: 14px;">
+                                            Next <i class="fas fa-arrow-right" style="margin-left: 10px;"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+
                         </div>
 
                         @include('frontend.00_android.00_fiturmenu.keterangan')
