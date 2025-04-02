0b144712bc4c88a7a13ab51a36d102ca60e0e8e5
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-02 19:25:39 +0700

    g

--- resources/views/frontend/00_android/02_satuanhargadasar/01_shstblora/index.blade.php
+++ resources/views/frontend/00_android/02_satuanhargadasar/01_shstblora/index.blade.php
@@ -1,17 +1,29 @@
 <style>
     /* Gaya untuk tabel */
-    .fl-table {
-        margin-top: 15px;
-        width: 100%;
-        border-collapse: collapse;
-        border-radius: 10px;
-    }
 
-    .fl-table th, .fl-table td {
-        text-align: center;
-        /* border-radius: 10px; */
-        /* padding: 10px; */
-    }
+    /* Membuat tabel dapat digeser secara horizontal */
+.table-wrapper {
+    width: 100%; /* Memastikan elemen pembungkus mengambil lebar penuh */
+    overflow-x: auto; /* Membuat konten dapat digulir secara horizontal */
+    margin-top: 10px; /* Menambah sedikit margin atas jika diperlukan */
+}
+
+.table-scroll {
+    overflow-x: auto;
+}
+
+.fl-table {
+    width: 100%;
+    border-collapse: collapse;
+    table-layout: fixed; /* Memastikan kolom tabel tidak tumpang tindih */
+}
+
+.fl-table th,
+.fl-table td {
+    padding: 8px;
+    text-align: center;
+}
+
 
     /* Gaya untuk kepala tabel */
     .fl-table thead {
@@ -109,20 +121,6 @@
                                 </tbody>
                             </table>
                             </div>
-
-                            {{-- <section class="document-section" style="margin-top: -215px; margin-bottom:35px;" >
-
-                            <div style="display: flex; justify-content: space-between; width: 100%; align-items: center; margin-top: 20px;">
-                                <!-- Tombol di kiri -->
-                                <a href="#" style="background: white;">
-                                    <button class="badgepupr" style="border: none; font-size: 12px; cursor: pointer;">
-                                        <img src="/assets/icon/pupr.png" class="img-fluid" alt="img-25" width="50" height="50" style="margin-right: 20px;" loading="lazy">
-                                        <i style="color: black" class="fas fa-building mr-2" style="margin-right: 10px;"></i> <span style="text-transform: uppercase; font-weight:800; color:black">Bangunan Gedung Rumah Negara Per M2 (Meter Persegi)</span>
-                                    </button>
-                                </a>
-                                <body>
-
-                                </section> --}}
 
                             <div class="table-wrapper" style="margin-top:-130px; margin-bottom:150px;">
                             <table class="fl-table">
