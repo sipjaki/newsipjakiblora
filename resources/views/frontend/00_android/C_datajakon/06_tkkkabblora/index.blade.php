fffe411ef544f62ed1a9b773a72b956b7fded4e9
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-04 23:00:16 +0700

    er

--- resources/views/frontend/00_android/C_datajakon/06_tkkkabblora/index.blade.php
+++ resources/views/frontend/00_android/C_datajakon/06_tkkkabblora/index.blade.php
@@ -158,7 +158,7 @@
                                         <tr>
                                             <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No </th>
                                             <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:1000px;"> Nama Lengkap </th>
-                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:700px;"> Jabatan Kerja</th>
+                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:900px;"> Jabatan Kerja</th>
                                             <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:500px;"> Penyelenggara/Asosiasi </th>
                                             <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:200px;"> View </th>
                                         </tr>
