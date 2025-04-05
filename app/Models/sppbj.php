c5b22d3d1c89cc829603bc8c970e88319e983c31
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-05 21:02:12 +0700

    e

--- app/Models/sppbj.php
+++ app/Models/sppbj.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class sppbj extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasApiTokens;
+
+    protected $guarded = ['id'];
+
+    public function paketpekerjaanmasjaki()
+    {
+        return $this->hasMany(paketpekerjaanmasjaki::class);
+    }
+
 }
