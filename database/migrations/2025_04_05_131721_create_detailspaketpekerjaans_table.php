c9b425536fea0bff81fac9838c90c9fe8ae52f6f
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-06 17:28:10 +0700

    er

--- database/migrations/2025_04_05_131721_create_detailspaketpekerjaans_table.php
+++ database/migrations/2025_04_05_131721_create_detailspaketpekerjaans_table.php
@@ -22,52 +22,62 @@ return new class extends Migration
             $table->date('tanggalpenetapan')->nullable();
             $table->date('tanggalsppj')->nullable();
             $table->string('nosppbj')->nullable();
+        //    5
             $table->string('nobahp')->nullable();
             $table->string('nonotadinas')->nullable();
             $table->date('tanggalnotadinas')->nullable();
             $table->string('nomorkontrak')->nullable();
             $table->string('harikontrak')->nullable();
+            // 10
             $table->string('tanggal')->nullable();
             $table->string('bulan')->nullable();
             $table->date('tanggalawal')->nullable();
             $table->date('tanggalakhir')->nullable();
             $table->date('tanggalakhirpemeliharaan')->nullable();
+            // 15
             $table->string('namadirektur')->nullable();
             $table->string('waktukontrak')->nullable();
             $table->string('terbilangwaktukontrak')->nullable();
             $table->string('waktupemeliharaan')->nullable();
             $table->string('terbilangpemeliharaan')->nullable();
+            // 20
             $table->string('jabatan')->nullable();
             $table->string('uangmuka')->nullable();
             $table->string('terbilang')->nullable();
             $table->string('penyedia')->nullable();
             $table->text('alamatcvpt')->nullable();
+            // 25
             $table->string('emailcvpt')->nullable();
             $table->string('website')->nullable();
             $table->string('notelepon')->nullable();
             $table->string('notaris')->nullable();
             $table->date('tanggalnotaris')->nullable();
+            // 30
             $table->string('kotanotaris')->nullable();
             $table->string('notarisperubahan')->nullable();
             $table->string('nomornotaris')->nullable();
             $table->date('tanggalnotaris')->nullable();
             $table->string('kodenotarisperubahan')->nullable();
+            // 35
             $table->string('nilaikontrak')->nullable();
             $table->string('terbilangnilaikontrak')->nullable();
             $table->string('nilaiuangmuka')->nullable();
             $table->string('dana')->nullable();
             $table->year('tahunanggaran')->nullable();
+            // 40
             $table->string('nospmk')->nullable();
             $table->string('tanggalspmk')->nullable();
             $table->string('harispmk')->nullable();
             $table->string('terbitspmk')->nullable();
             $table->string('nospl')->nullable();
+            // 45
             $table->string('nodpa')->nullable();
             $table->string('tanggaldpa')->nullable();
             $table->string('rekening')->nullable();
             $table->string('namabank')->nullable();
             $table->string('bank')->nullable();
             $table->string('konsultanpengawas')->nullable();
+            // 50
             $table->softDeletes();
             $table->timestamps();
         });
