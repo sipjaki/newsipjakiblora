60671eb35fed480d03c6568dfbfb6f73ac428702
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-06 15:30:29 +0700

    re

--- database/migrations/2025_03_15_131721_create_paketpekerjaanmasjakis_table.php
+++ database/migrations/2025_03_15_131721_create_paketpekerjaanmasjakis_table.php
@@ -28,7 +28,7 @@ return new class extends Migration
             $table->foreignId('user_id')->nullable();
             $table->foreignId('bulanrekap_id')->nullable();
             // ----------------------------------------------------------------
-            $table->string('namapekerjaan')->nullable();
+            $table->text('namapekerjaan')->nullable();
             $table->string('cvptpenyedia')->nullable();
             $table->string('nib')->nullable();
             $table->decimal('nilaikontrak', 15, 2)->nullable(); // Menggunakan tipe decimal untuk nilai kontrak
