3980588c92cfbfd3d1b5290091bc3f9a58960c9b
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-07 01:59:11 +0700

    er

--- resources/views/frontend/00_android/01_artikeljakon/index.blade.php
+++ resources/views/frontend/00_android/01_artikeljakon/index.blade.php
@@ -3,7 +3,7 @@
 <body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
     <section class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#FCF7F1] overflow-x-hidden pb-4">
         {{-- <div class="header flex flex-col" style="background: linear-gradient(to bottom, #28A745, #FFD100); border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden;"> --}}
-            <div class="header flex flex-col" style="background-image: url('/assets/00_android/iconmenu/menuutama1.jpg'); background-size: cover; background-position: center; border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden; height: 500px;">
+            <div class="header flex flex-col" style="background-image: url('/assets/00_android/iconmenu/menuutama1.jpg'); background-size: cover; background-position: center; border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden; height: 300px;">
 
                 <nav class="pt-5 px-3 flex justify-between items-center">
                     <!-- Logo bagian kiri -->
@@ -28,7 +28,7 @@
                 </nav>
             </div>
 
-                    <div class="flex flex-col gap-4 px-4">
+                    <div class="flex flex-col gap-4 px-4" style="margin-top: -100px;">
                         <br><br>
             @foreach ($data as $item)
             <a href="/resartikeljakon/{{$item->judul}}" class="card">
