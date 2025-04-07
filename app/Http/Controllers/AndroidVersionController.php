cf608d947df59af6f8ad8406389b22ecb6f12195
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-07 18:12:01 +0700

    re

--- app/Http/Controllers/AndroidVersionController.php
+++ app/Http/Controllers/AndroidVersionController.php
@@ -596,19 +596,6 @@ class AndroidVersionController extends Controller
         {
             $dataagendaskk = agendaskk::where('namakegiatan', $namakegiatan)->first();
 
-            // if (!$databujkkontraktor) {
-            //     // Tangani jika kegiatan tidak ditemukan
-            //     return redirect()->back()->with('error', 'Kegiatan tidak ditemukan.');
-            // }
-
-            // // Menggunakan paginate() untuk pagination
-            // $subdata = bujkkontraktorsub::where('bujkkontraktor_id', $databujkkontraktor->id)->paginate(50);
-
-            //   // Menghitung nomor urut mulai
-            //     $start = ($subdata->currentPage() - 1) * $subdata->perPage() + 1;
-
-
-        // Ambil data user saat ini
         $user = Auth::user();
         $datamateripelatihan = materipelatihan::all();
 
