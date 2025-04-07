af58d84f935d6b8be5ec2d3fab58f2f6de89acff
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-07 12:15:57 +0700

    g

--- resources/views/frontend/00_android/G_rantaipasok/index.blade.php
+++ resources/views/frontend/00_android/G_rantaipasok/index.blade.php
@@ -3,7 +3,8 @@
 <body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
     <section class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#FCF7F1] overflow-x-hidden pb-4">
         {{-- <div class="header flex flex-col" style="background: linear-gradient(to bottom, #28A745, #FFD100); border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden;"> --}}
-            <div class="header flex flex-col bg-[#56BBC5] rounded-b-[50px] overflow-hidden h-[320px] -mb-[181px]" style="background: linear-gradient(to bottom, #28A745, #FFD100);">
+            <div class="header flex flex-col" style="background-image: url('/assets/00_android/iconmenu/menuutama1.jpg'); background-size: cover; background-position: center; border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden; height: 350px;">
+
                 <nav class="pt-5 px-3 flex justify-between items-center">
                     <!-- Logo bagian kiri -->
                     <div class="flex items-center gap-[10px]">
@@ -27,9 +28,7 @@
                 </nav>
             </div>
 
-                    <div class="flex flex-col gap-4 px-4">
-                        <br><br>
-
+                    <div class="flex flex-col gap-4 px-4" style="margin-top: -100px;">
                             <a href="/404" class="card">
                             <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
                                 <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
