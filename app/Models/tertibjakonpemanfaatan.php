e08f71eeed0d9506f5b09d5bb30967f76d5c63d7
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-08 17:52:11 +0700

    e

--- app/Models/tertibjakonpemanfaatan.php
+++ app/Models/tertibjakonpemanfaatan.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -13,31 +12,33 @@ class tertibjakonpemanfaatan extends Model
 
     protected $guarded = ['id'];
 
+    // Relasi dengan penyediastatustertibjakon
     public function penyediastatustertibjakon()
     {
-        return $this->belongsTo(penyediastatustertibjakon::class);
+        return $this->belongsTo(penyediastatustertibjakon::class); // Relasi yang benar
     }
 
+    // Relasi dengan surattertibjakonpemanfaatan1
     public function surattertibjakonpemanfaatan1()
     {
-        return $this->belongsTo(surattertibjakonpemanfaatan1::class);
+        return $this->belongsTo(surattertibjakonpemanfaatan1::class); // Ganti dengan kelas yang sesuai
     }
 
+    // Relasi dengan surattertibjakonpemanfaatan2
     public function surattertibjakonpemanfaatan2()
     {
         return $this->belongsTo(surattertibjakonpemanfaatan2::class);
     }
 
+    // Relasi dengan surattertibjakonpemanfaatan3
     public function surattertibjakonpemanfaatan3()
     {
         return $this->belongsTo(surattertibjakonpemanfaatan3::class);
     }
 
+    // Relasi dengan surattertibjakonpemanfaatan4
     public function surattertibjakonpemanfaatan4()
     {
         return $this->belongsTo(surattertibjakonpemanfaatan4::class);
     }
-
 }
-
-
