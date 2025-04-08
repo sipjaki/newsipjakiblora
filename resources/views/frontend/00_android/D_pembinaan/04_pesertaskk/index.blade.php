8ac714c1428a4d6f8dce3f9561739f662a460db9
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-08 14:09:37 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/04_pesertaskk/index.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/04_pesertaskk/index.blade.php
@@ -214,9 +214,9 @@ h5 {
                                     <thead>
                                         <tr>
                                             <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No </th>
-                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:300px;"> Nama Kegiatan </th>
-                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:200px;"> Kuota </th>
-                                            <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Keterangan </th>
+                                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:300px;"> Nama_Kegiatan </th>
+                                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:200px;"> Kuota_Peserta </th>
+                                            <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Keterangan_Kegiatan </th>
                                             <th style="text-align:center; width:100px;"> View Peserta </th>
                                         </tr>
                                     </thead>
