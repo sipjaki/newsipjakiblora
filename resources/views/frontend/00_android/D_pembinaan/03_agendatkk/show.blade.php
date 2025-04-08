96698246d5b253a5c532f15c93be28df4879e04c
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-08 09:41:01 +0700

    er

--- resources/views/frontend/00_android/D_pembinaan/03_agendatkk/show.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/03_agendatkk/show.blade.php
@@ -194,7 +194,7 @@ h5 {
                     </div>
 
                     <br>
-                    <h4 style="font-weight:bold;">I. INFORMASI AGENDA PELATIHAN</h4>
+                    <h4 style="font-weight:bold;">I. INFORMASI AGENDA SERTIFIKASI </h4>
                     <table class="table-identitas">
                         {{-- @foreach ($data as $item) --}}
                         <tr>
@@ -247,7 +247,7 @@ h5 {
 
                     <br>
 
-                    <h4 style="font-weight:bold;">II. DOWNLOAD MATERI PELATIHAN </h4>
+                    <h4 style="font-weight:bold;">II. DOWNLOAD MATERI SERTIFIKASI </h4>
                     <div style="margin: 20px; padding: 15px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9;">
                         <table class="custom-fl-table" id="sortableTable">
                             <thead>
