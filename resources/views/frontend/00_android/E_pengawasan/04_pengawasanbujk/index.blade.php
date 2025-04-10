00b946be74baea2cc7242ba697c3ed9237eb1f1c
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-10 13:02:24 +0700

    e

--- resources/views/frontend/00_android/E_pengawasan/04_pengawasanbujk/index.blade.php
+++ resources/views/frontend/00_android/E_pengawasan/04_pengawasanbujk/index.blade.php
@@ -130,7 +130,7 @@
                                 function searchTable() {
                                 let input = document.getElementById("searchInput").value;
 
-                                fetch(`/resjakonpenyelenggaraan?search=${input}`)
+                                fetch(`/respengawasanbujk?search=${input}`)
                                     .then(response => response.text())
                                     .then(html => {
                                         let parser = new DOMParser();
