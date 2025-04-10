c0425d4000a2db5998b27c1ae35b11ee9055e129
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-10 13:10:57 +0700

    erg

--- resources/views/frontend/00_android/E_pengawasan/05_kecelakaankerja/index.blade.php
+++ resources/views/frontend/00_android/E_pengawasan/05_kecelakaankerja/index.blade.php
@@ -130,7 +130,7 @@
                                 function searchTable() {
                                 let input = document.getElementById("searchInput").value;
 
-                                fetch(`/respengawasanbujk?search=${input}`)
+                                fetch(`/reskecelakaankerja?search=${input}`)
                                     .then(response => response.text())
                                     .then(html => {
                                         let parser = new DOMParser();
