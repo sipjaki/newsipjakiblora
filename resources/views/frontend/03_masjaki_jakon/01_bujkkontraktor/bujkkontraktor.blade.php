2df3d87521a2a0b3a78df658f2c83db12c3cb461
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-16 01:00:43 +0700

    eg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -188,6 +188,11 @@ table.zebra-table {
                                     <button class="btn-navy">Data Belum Diupdate</button>
                                 @endif
                             </td>
+                            <td style="text-align: center">
+                                <a href="/datajakon/bujkkontraktor/{{$item->namalengkap}}">
+                                    <i class="fas fa-eye view-icon" onclick="alert('View clicked!')"></i>
+                                </a>
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
