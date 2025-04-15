9eea8520dcd67748d6b566287a4ffae790cdeb2e
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-16 01:44:42 +0700

    eg

--- resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/tenagakerjakonstruksi.blade.php
+++ resources/views/frontend/03_masjaki_jakon/03_tenagakerjakonstruksi/tenagakerjakonstruksi.blade.php
@@ -187,11 +187,11 @@ table.zebra-table {
 
                             <td style="text-align: center;">
                                 @if(!empty($item->statusterbit))
-                                    <button class="btn btn-warning" style="padding: 1px 6px; font-size: 12px; border-radius: 4px; line-height: 1;">
+                                    <button class="btn btn-warning" style="padding: 2px 6px; font-size: 11px; border-radius: 3px; line-height: 1.1;">
                                         {{ $item->statusterbit ?? 'Data Tidak Ditemukan' }}
                                     </button>
                                 @else
-                                    <button class="btn btn-navy" style="padding: 1px 6px; font-size: 12px; border-radius: 4px; line-height: 1;">
+                                    <button class="btn btn-navy" style="padding: 2px 6px; font-size: 11px; border-radius: 3px; line-height: 1.1;">
                                         Data Belum Diupdate
                                     </button>
                                 @endif
