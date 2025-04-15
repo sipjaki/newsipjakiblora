a131cc2d71e8fd64a4070488404662a4df564421
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-16 00:12:53 +0700

    eg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktor.blade.php
@@ -136,7 +136,7 @@ table.zebra-table {
     </div>
 
     <!-- Konten Data -->
-    <div class="w-[75%] mx-auto pb-10">
+    <div class="w-[50%] mx-auto pb-10">
         <div class="bg-white flex flex-col gap-5 p-5 rounded-[20px] shadow-md w-full">
             <div class="table-wrapper">
                 <table class="zebra-table w-full">
