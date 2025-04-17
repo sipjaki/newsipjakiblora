ed8c552b997a282332f2cac59b198dc521119e53
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-18 01:14:38 +0700

    eg

--- resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
+++ resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
@@ -212,7 +212,7 @@ table.zebra-table {
 
             </div>
 
-            @include('frontend.00_approve.01_cssterpisah.paginations')
+            {{-- @include('frontend.00_approve.01_cssterpisah.paginations') --}}
         </div>
     </div>
     <br><br><br>
