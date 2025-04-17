521ba32a14fdd671a3a3d3623d98160934a96bec
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-17 21:32:42 +0700

    eg

--- resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
+++ resources/views/frontend/03_masjaki_jakon/01_bujkkontraktor/bujkkontraktorshow.blade.php
@@ -179,7 +179,7 @@ table.zebra-table {
 
 
 
-  <section id="details" class="container mx-auto flex flex-col sm:flex-row gap-5" style="margin-top: -100px;">
+  <section id="details" class="container mx-auto flex flex-col sm:flex-row gap-5" style="margin-top: -10px;">
     <div class="flex flex-col gap-5 w-full">
         <div class="bg-white flex flex-col gap-5 p-5 rounded-[20px] w-full">
             <div class="flex justify-center">
