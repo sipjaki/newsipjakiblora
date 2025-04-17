6d210bf464e31acd86b2bfcbdfc49043d83ff264
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-17 10:31:16 +0700

    er

--- resources/views/frontend/04_pembinaan/01_agendapembinaan/index.blade.php
+++ resources/views/frontend/04_pembinaan/01_agendapembinaan/index.blade.php
@@ -181,20 +181,21 @@ table.zebra-table {
 
                 <table class="zebra-table fl-table" id="sortableTable" style="margin-top: 15px; width: 100%; border-collapse: collapse;">
                     <thead>
-                            <tr>
-                                <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:200px;"> Kategori Pelatihan <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:300px;"> Nama Kegiatan <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:250px;"> Penyelenggara <span class="sort-icon">⇅</span></th>
-                                {{-- <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:150px;"> Jenjang <span class="sort-icon">⇅</span></th> --}}
-                                <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:200px;"> Penutupan <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:200px;"> Waktu Pelaksanaan <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(6)" style="cursor:pointer; text-align:center; width:150px;"> Jumlah Peserta <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(7)" style="cursor:pointer; text-align:center; width:250px;"> Lokasi <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Keterangan <span class="sort-icon">⇅</span></th>
-                                <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Pendaftaran <span class="sort-icon">⇅</span></th>
-                                {{-- <th style="text-align:center; width:100px;"> Daftar </th> --}}
-                            </tr>
+                        <tr>
+                            <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No </th>
+                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:200px;"> Kategori Pelatihan </th>
+                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:300px;"> Nama Kegiatan </th>
+                            <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:250px;"> Penyelenggara </th>
+                            {{-- <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:150px;"> Jenjang </th> --}}
+                            <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:200px;"> Penutupan </th>
+                            <th onclick="sortTable(5)" style="cursor:pointer; text-align:center; width:200px;"> Waktu Pelaksanaan </th>
+                            <th onclick="sortTable(6)" style="cursor:pointer; text-align:center; width:150px;"> Jumlah Peserta </th>
+                            <th onclick="sortTable(7)" style="cursor:pointer; text-align:center; width:250px;"> Lokasi </th>
+                            <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Keterangan </th>
+                            <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Pendaftaran </th>
+                            {{-- <th style="text-align:center; width:100px;"> Daftar </th> --}}
+                        </tr>
+
 
                     </thead>
 
