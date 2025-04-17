959ac48e8dfc228c9d0c8024b4f44f1840cd46d9
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-18 01:16:43 +0700

    eg

--- resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
+++ resources/views/frontend/04_pembinaan/01_agendapembinaan/daftarpesertashow.blade.php
@@ -184,10 +184,10 @@ table.zebra-table {
                 <table class="zebra-table fl-table" id="sortableTable" style="margin-top: 15px; width: 100%; border-collapse: collapse;">
                     <thead>
                         <tr>
-                            <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No <span class="sort-icon">⇅</span></th>
-                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:300px;"> Nama Lengkap <span class="sort-icon">⇅</span></th>
-                            <th onclick="sortTable(2)" style="cursor:pointer; text-align:center; width:100;"> Gender <span class="sort-icon">⇅</span></th>
-                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:400px;"> Instansi <span class="sort-icon">⇅</span></th>
+                            <th onclick="sortTable(0)" style="cursor:pointer; text-align:center; width:100px;"> No </th>
+                            <th onclick="sortTable(1)" style="cursor:pointer; text-align:center;"> Nama Lengkap </th>
+                            <th onclick="sortTable(2)" style="cursor:pointer; text-align:center;"> Gender </th>
+                            <th onclick="sortTable(3)" style="cursor:pointer; text-align:center;"> Instansi </th>
                             {{-- <th style="text-align:center; width:100px;"> View </th> --}}
                         </tr>
                     </thead>
