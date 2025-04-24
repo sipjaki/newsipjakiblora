0a0a848865a62fca588bcfb8df927b7a74f91173
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-25 00:24:35 +0700

    ge

--- resources/views/backend/02_kelembagaan/03_tupoksi/update.blade.php
+++ resources/views/backend/02_kelembagaan/03_tupoksi/update.blade.php
@@ -19,9 +19,8 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
-
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
             </div>
             <!--end::Row-->
           </div>
@@ -31,12 +30,6 @@
         <br>
         <!-- Menampilkan pesan sukses -->
 
-        {{-- ======================================================= --}}
-        {{-- ALERT --}}
-
-        @include('backend.00_administrator.00_baganterpisah.06_alert')
-
-        {{-- ======================================================= --}}
 
             <!-- Menyertakan FontAwesome untuk ikon -->
 
@@ -46,28 +39,11 @@
                 <!-- /.card -->
                 <div class="card mb-4">
                     <div class="card-header">
-                        <div style="
-                        margin-bottom:10px;
-                        font-weight: 900;
-                        font-size: 16px;
-                        text-align: center;
-                        background: linear-gradient(135deg, #1f50f1, #1f50f1);
-                        color: white;
-                        padding: 10px 25px;
-                        border-radius: 10px;
-                        display: inline-block;
-                        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                        width: 100%;
-                    ">
-                        📌 Halaman Update : {{$title}}
-
-                    </div>
+                        @include('backend.00_administrator.00_baganterpisah.12_judulupdate')
 
                            {{-- ======================================================= --}}
         {{-- ALERT --}}
 
-
-        @include('backend.00_administrator.00_baganterpisah.06_alert')
 
 
         <div class="card card-primary card-outline mb-6">
