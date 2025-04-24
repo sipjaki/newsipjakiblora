ce17990d17e4605509fc41f450bf33ed99ff453e
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 22:34:38 +0700

    eg

--- resources/views/backend/02_kelembagaan/02_profiljakon/03_kepaladinas/index.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/03_kepaladinas/index.blade.php
@@ -20,7 +20,8 @@
             <!--begin::Row-->
             <div class="row">
 
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
 
             </div>
             <!--end::Row-->
@@ -45,23 +46,7 @@
             <div class="row" style="margin-right: 10px; margin-left:10px;">
                 <!-- /.card -->
                 <div class="card mb-4">
-                    <div class="card-header">
-                        <div style="
-                        margin-bottom:10px;
-                        font-weight: 900;
-                        font-size: 16px;
-                        text-align: center;
-                        background: linear-gradient(135deg, #166534, #166534);
-                        color: white;
-                        padding: 10px 25px;
-                        border-radius: 10px;
-                        display: inline-block;
-                        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                        width: 100%;
-                    ">
-                        📌 Halaman : {{$title}}
-                    </div>
-
+                    @include('backend.00_administrator.00_baganterpisah.10_judulhalaman')
 
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
