b42c497f51361c9ed457a6441298e6479d2d8686
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 14:25:14 +0700

    eg

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
@@ -44,17 +44,17 @@
 
 
         <div class="card card-primary card-outline mb-6">
-            <div style="display: flex; justify-content: flex-end; margin-top:10px;">
-                <a href="/bestrukturdinas">
+            <div style="display: flex; justify-content: flex-end; margin-top: 10px;">
+                <a href="/bestrukturdinas" style="text-decoration: none;">
                     <button
-                    onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                    onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
-                    style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                    <!-- Ikon Kembali -->
-                    <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
-                    Kembali
-                </button>
-            </a>
+                        onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                        onmouseout="this.style.backgroundColor='#9CA3AF'; this.style.color='white';"
+                        style="background-color: #9CA3AF; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; font-family: 'Poppins', sans-serif;">
+                        <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
+                        Kembali
+                    </button>
+                </a>
+            </div>
         </div>
         <hr>
 
