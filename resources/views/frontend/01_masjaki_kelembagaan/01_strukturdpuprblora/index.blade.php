ee3c5873dcd2dd4099e728c537043448746d50c8
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 23:57:58 +0700

    eg

--- resources/views/frontend/01_masjaki_kelembagaan/01_strukturdpuprblora/index.blade.php
+++ resources/views/frontend/01_masjaki_kelembagaan/01_strukturdpuprblora/index.blade.php
@@ -166,10 +166,10 @@ table.zebra-table {
                     <div style="margin-top: 10px;">
                         @if($item->peraturan && file_exists(public_path('storage/' . $item->peraturan)))
                         <!-- Display the default iframe when the file exists in the storage -->
-                        <iframe src="{{ asset('storage/' . $item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                        <iframe src="{{ asset('storage/' . $item->peraturan) }}" frameborder="0" width="100%" height="800px"></iframe>
                     @elseif($item->peraturan)
                         <!-- Display the iframe with the updated file -->
-                        <iframe src="{{ asset($item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                        <iframe src="{{ asset($item->peraturan) }}" frameborder="0" width="100%" height="800px"></iframe>
                     @else
                         <!-- Optional: Show a placeholder if there's no file available -->
                         <p>Data belum diupdate</p>
