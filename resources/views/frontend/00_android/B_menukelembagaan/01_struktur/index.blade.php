d8a7a0911aa376add51b0bedd78ad63409d6d44c
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-25 00:16:42 +0700

    eg

--- resources/views/frontend/00_android/B_menukelembagaan/01_struktur/index.blade.php
+++ resources/views/frontend/00_android/B_menukelembagaan/01_struktur/index.blade.php
@@ -33,7 +33,22 @@
                     {{-- <img src="assets/image/portfolio/portfolio-details-1.jpg" class="img-fluid" alt="img-109"> --}}
 
                     @foreach ($data as $item )
-                    <iframe src="{{ asset('storage/' . $item->peraturan) }}" width="100%" height="750" style="border: none; margin-bottom:25px;"></iframe>
+
+                    <div style="margin-top: 10px;">
+                        @if($item->peraturan && file_exists(public_path('storage/' . $item->peraturan)))
+                        <!-- Display the default iframe when the file exists in the storage -->
+                        <iframe src="{{ asset('storage/' . $item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                    @elseif($item->peraturan)
+                        <!-- Display the iframe with the updated file -->
+                        <iframe src="{{ asset($item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                    @else
+                        <!-- Optional: Show a placeholder if there's no file available -->
+                        <p>Data belum diupdate</p>
+                    @endif
+
+                    </div>
+
+
                     <div class="portfolio-details-content">
                         <div class="flex flex-col gap-[2px]">
                             <h2 class="font-semibold text-sm">Redaksi : </h2>
