ef057ac0393305edbf3ba337e9659dc30c1e1647
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 15:30:45 +0700

    eg

--- resources/views/backend/02_kelembagaan/02_profiljakon/01_judul/index.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/01_judul/index.blade.php
@@ -18,8 +18,9 @@
           <!--begin::Container-->
           <div class="container-fluid">
             <!--begin::Row-->
+            <div class="row">
 
-            @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
 
             </div>
             <!--end::Row-->
@@ -44,9 +45,23 @@
             <div class="row" style="margin-right: 10px; margin-left:10px;">
                 <!-- /.card -->
                 <div class="card mb-4">
-                    @include('backend.00_administrator.00_baganterpisah.10_judulhalaman')
-
-                        <!-- /.card-header -->
+                    <div class="card-header">
+                        <div style="
+                        margin-bottom:10px;
+                        font-weight: 900;
+                        font-size: 16px;
+                        text-align: center;
+                        background: linear-gradient(135deg, #166534, #166534);
+                        color: white;
+                        padding: 10px 25px;
+                        border-radius: 10px;
+                        display: inline-block;
+                        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+                        width: 100%;
+                    ">
+                        📌 Halaman : {{$title}}
+                    </div>
+                    <!-- /.card-header -->
                     <div class="card-body p-0">
                         <div class="table-responsive">
                             <table class="table table-striped">
