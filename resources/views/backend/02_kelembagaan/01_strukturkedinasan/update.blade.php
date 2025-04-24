4ae17390804927799b41ebfc3a0a44a1b1c2560b
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 14:44:31 +0700

    e

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
@@ -129,7 +129,7 @@
                                     onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                                     onmouseout="this.style.backgroundColor='#189200'; this.style.color='white';"
                                     style="background-color: #189200; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                                    <i class="fa fa-edit" style="margin-right: 8px;"></i> Update
+                                    <i class="fa fa-edit" style="margin-right: 8px;"></i><span style="font-family: 'Poppins', sans-serif;">Update</span>
                                 </button>
                             </div>
                         </form>
