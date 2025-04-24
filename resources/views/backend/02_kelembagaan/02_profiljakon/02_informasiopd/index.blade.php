c482164e015ef98aa785e87c2f94d29d5b99756a
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 17:04:32 +0700

    eg

--- resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/index.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/02_informasiopd/index.blade.php
@@ -20,7 +20,8 @@
             <!--begin::Row-->
             <div class="row">
 
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
 
             </div>
             <!--end::Row-->
@@ -30,13 +31,6 @@
 
         <br>
         <!-- Menampilkan pesan sukses -->
-
-        {{-- ======================================================= --}}
-        {{-- ALERT --}}
-
-        @include('backend.00_administrator.00_baganterpisah.06_alert')
-
-        {{-- ======================================================= --}}
 
             <!-- Menyertakan FontAwesome untuk ikon -->
 
@@ -184,14 +178,20 @@
                             Update
                         </button>
                         </a>
-                        <a href="/beprofiljakon">
-                            <button
+                    <a href="/beprofiljakon">
+                        <button
                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                             onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
                             style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                            <!-- Ikon Kembali -->
-                            <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
-                            Kembali
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 viewBox="0 0 16 16" style="margin-right: 8px;">
+                              <path fill-rule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
+                            </svg>
+
+                            <span style="font-family: 'Poppins', sans-serif; font-size: 16px; line-height: 1.5;">
+                                Kembali
+                            </span>
                         </button>
                     </a>
                 </div>
