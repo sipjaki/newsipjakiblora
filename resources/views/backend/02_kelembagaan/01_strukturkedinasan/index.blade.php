5fa86a53f5597e344aafefbeb0f4aa940f47ff4e
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 14:48:52 +0700

    eg

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/index.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/index.blade.php
@@ -132,7 +132,8 @@ table.zebra-table {
                                         <td style="text-align: center;">{{ $loop->iteration }}</td>
                                         <td style="text-align: left;">{!! $item->judul !!}</td>
                                         <td style="text-align: center;">
-                                            <iframe src="{{ asset('storage/' . $item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                                            {{-- <iframe src="{{ asset('storage/' . $item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe> --}}
+                                            <iframe src="{{ asset($item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
                                         </td>
                                         <td style="text-align: justify;">{!! $item->keterangan !!}</td>
                                         <td style="text-align: center;">
