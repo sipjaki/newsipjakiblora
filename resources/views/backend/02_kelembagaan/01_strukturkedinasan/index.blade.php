10bb842980e67a68898b10ea6c951ff70492b8f5
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 13:59:37 +0700

    eg

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/index.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/index.blade.php
@@ -20,7 +20,16 @@
             <!--begin::Row-->
             <div class="row">
 
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+                <div class="col-sm-12">
+                    <h3 class="mb-0" style="font-family: 'Poppins', sans-serif;">
+                      Selamat datang!
+                      <span style="color: black; font-weight:800;">{{ Auth::user()->name }}</span>
+                      di Dashboard
+                      <span style="color: black; font-weight:800;">{{ Auth::user()->statusadmin->statusadmin }}</span>
+                      Sistem Informasi Pembina Jasa Konstruksi Kab Blora
+                    </h3>
+                  </div>
+
 
             </div>
             <!--end::Row-->
