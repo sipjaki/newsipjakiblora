d9fef0c7ee9ecfeab3bfecef475488f7874e2345
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 23:16:38 +0700

    eg

--- resources/views/backend/02_kelembagaan/02_profiljakon/07_personilsipjaki/index.blade.php
+++ resources/views/backend/02_kelembagaan/02_profiljakon/07_personilsipjaki/index.blade.php
@@ -19,8 +19,8 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
 
             </div>
             <!--end::Row-->
@@ -46,21 +46,7 @@
                 <!-- /.card -->
                 <div class="card mb-4">
                     <div class="card-header">
-                        <div style="
-                        margin-bottom:10px;
-                        font-weight: 900;
-                        font-size: 16px;
-                        text-align: center;
-                        background: linear-gradient(135deg, #166534, #166534);
-                        color: white;
-                        padding: 10px 25px;
-                        border-radius: 10px;
-                        display: inline-block;
-                        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                        width: 100%;
-                    ">
-                        📌 Halaman : {{$title}}
-                    </div>
+                        @include('backend.00_administrator.00_baganterpisah.10_judulhalaman')
 
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
@@ -146,7 +132,9 @@
                             onmouseout="this.style.backgroundColor='#117235'; this.style.color='white';"
                             style="background-color: #117235; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                             <!-- Ikon Kembali -->
-                            <i class="fa fa-file" style="margin-right: 8px;"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 384 512" fill="white" style="margin-right: 8px;">
+                                <path d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zM384 121.9V128H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z"/>
+                            </svg>
                             Update
                         </button>
                         </a>
@@ -156,7 +144,10 @@
                             onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
                             style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
                             <!-- Ikon Kembali -->
-                            <i class="fa fa-arrow-left" style="margin-right: 8px;"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            viewBox="0 0 16 16" style="margin-right: 8px;">
+                         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
+                       </svg>
                             Kembali
                         </button>
                     </a>
