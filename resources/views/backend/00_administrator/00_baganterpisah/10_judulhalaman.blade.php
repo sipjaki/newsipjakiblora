e6b69ba2c2e007328ecdddc6f703778f0b850090
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 16:52:43 +0700

    eg

--- resources/views/backend/00_administrator/00_baganterpisah/10_judulhalaman.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/10_judulhalaman.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
 <div class="card mb-4">
     <div class="card-header">
       <div class="halaman-title">
