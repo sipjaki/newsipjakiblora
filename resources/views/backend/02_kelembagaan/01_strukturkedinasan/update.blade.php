faadf563763c37d4f3184156572d90033db16627
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 15:03:22 +0700

    eg

--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/update.blade.php
@@ -103,12 +103,12 @@
 
                                             <!-- Preview PDF -->
                                             <div style="margin-top: 10px;">
-                                                @if($item->peraturan && file_exists(public_path('storage/' . $item->peraturan)))
+                                                @if($data->peraturan && file_exists(public_path('storage/' . $data->peraturan)))
                                                 <!-- Display the default iframe when the file exists in the storage -->
-                                                <iframe src="{{ asset('storage/' . $item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
-                                            @elseif($item->peraturan)
+                                                <iframe src="{{ asset('storage/' . $data->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                                            @elseif($data->peraturan)
                                                 <!-- Display the iframe with the updated file -->
-                                                <iframe src="{{ asset($item->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
+                                                <iframe src="{{ asset($data->peraturan) }}" frameborder="0" width="100%" height="300px"></iframe>
                                             @else
                                                 <!-- Optional: Show a placeholder if there's no file available -->
                                                 <p>Data belum diupdate</p>
