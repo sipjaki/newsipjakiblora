b7d689dce517b759380b7471bd427f358f0673da
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-24 14:03:12 +0700

    eg

---
+++ resources/views/backend/00_administrator/00_baganterpisah/09_selamatdatang.blade.php
@@ -0,0 +1,18 @@
+<div class="container-fluid">
+    <!--begin::Row-->
+    <div class="row">
+    <!-- Kontennya -->
+    <div class="col-sm-12">
+        <h3 class="mb-0" style="font-family: 'Poppins', sans-serif; font-size: 18px;">
+        Selamat datang!
+        <span style="color: black; font-weight:800;">{{ Auth::user()->name }}</span>
+        di Dashboard
+        <span style="color: black; font-weight:800;">{{ Auth::user()->statusadmin->statusadmin }}</span>
+        Sistem Informasi Pembina Jasa Konstruksi Kab Blora
+        </h3>
+    </div>
+
+
+    </div>
+    <!--end::Row-->
+  </div>
--- resources/views/backend/02_kelembagaan/01_strukturkedinasan/index.blade.php
+++ resources/views/backend/02_kelembagaan/01_strukturkedinasan/index.blade.php
@@ -13,27 +13,13 @@
 
       <!--begin::App Main-->
       <main class="app-main">
-        <!--begin::App Content Header-->
-        <div class="app-content-header">
-          <!--begin::Container-->
-          <div class="container-fluid">
-            <!--begin::Row-->
-            <div class="row">
-            <!-- Kontennya -->
-            <div class="col-sm-12">
-                <h3 class="mb-0" style="font-family: 'Poppins', sans-serif; font-size: 18px;">
-                Selamat datang!
-                <span style="color: black; font-weight:800;">{{ Auth::user()->name }}</span>
-                di Dashboard
-                <span style="color: black; font-weight:800;">{{ Auth::user()->statusadmin->statusadmin }}</span>
-                Sistem Informasi Pembina Jasa Konstruksi Kab Blora
-                </h3>
-            </div>
+          <!--begin::App Content Header-->
+          <div class="app-content-header">
 
+              <!--begin::Container-->
 
-            </div>
-            <!--end::Row-->
-          </div>
+              @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+
           <!--end::Container-->
         </div>
 
