c8e13b9a0da87407a0a0b3e4ef8d0e9e40a93606
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-26 14:03:01 +0700

    eg

--- resources/views/backend/00_administrator/00_baganterpisah/01_header.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/01_header.blade.php
@@ -66,8 +66,7 @@
     <link rel="icon" href="/assets/icon/logokabupatenblora.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <style>
         * {
