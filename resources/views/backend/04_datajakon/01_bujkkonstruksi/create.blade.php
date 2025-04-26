d8b0388af69479bc1ef9360bfa168b21b017bf2e
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-26 13:42:26 +0700

    gew

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/create.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/create.blade.php
@@ -22,8 +22,8 @@
             <!--begin::Row-->
             <div class="row">
 
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
-
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
             </div>
             <!--end::Row-->
           </div>
@@ -31,16 +31,6 @@
         </div>
 
         <br>
-        <!-- Menampilkan pesan sukses -->
-
-        {{-- ======================================================= --}}
-        {{-- ALERT --}}
-
-        @include('backend.00_administrator.00_baganterpisah.06_alert')
-
-        {{-- ======================================================= --}}
-
-            <!-- Menyertakan FontAwesome untuk ikon -->
 
         <div class="container-fluid">
             <!--begin::Row-->
@@ -48,29 +38,10 @@
                 <!-- /.card -->
                 <div class="card mb-4">
                     <div class="card-header">
-                        <div style="
-                            margin-bottom: 10px;
-                            font-weight: 900;
-                            font-size: 16px;
-                            text-align: center;
-                            background: linear-gradient(135deg, #D5006D, #4CAF50); /* Fuchsia ke Hijau */
-                            color: white;
-                            padding: 10px 25px;
-                            border-radius: 10px;
-                            display: inline-block;
-                            box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                            width: 100%;
-                        ">
-                            📌 Halaman Tambah Data : {{$title}}
-                        </div>
+                        @include('backend.00_administrator.00_baganterpisah.13_judulcreate')
                     </div>
 
                            {{-- ======================================================= --}}
-        {{-- ALERT --}}
-
-
-        @include('backend.00_administrator.00_baganterpisah.06_alert')
-
 
         <div class="card card-primary card-outline mb-6">
             <div style="display: flex; justify-content: flex-end; margin-top:10px;">
