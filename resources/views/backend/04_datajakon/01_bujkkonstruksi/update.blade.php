4df4a1cbe2209e571cad36726516f9739ec12b50
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-26 15:43:51 +0700

    eg

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/update.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/update.blade.php
@@ -105,7 +105,7 @@
                                             <label class="form-label" for="user_id">
                                                 <i class="bi bi-building" style="margin-right: 10px; color: navy;"></i> Administrator
                                             </label>
-                                            <input type="text" id="user_id" name="user_id" class="form-control @error('user_id') is-invalid @enderror" value="{{ old('user_id', $data->user->name) }}" />
+                                            <input type="text" id="user_id" name="user_id" class="form-control @error('user_id') is-invalid @enderror" value="{{ old('user_id', $data->user->name) }}" readonly/>
                                             @error('user_id')
                                                 <div class="invalid-feedback">{{ $message }}</div>
                                             @enderror
