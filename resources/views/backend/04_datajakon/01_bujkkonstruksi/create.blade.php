076ac24c1da91e19d6b5c20906efd228db0e3126
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-26 13:15:22 +0700

    eg

--- resources/views/backend/04_datajakon/01_bujkkonstruksi/create.blade.php
+++ resources/views/backend/04_datajakon/01_bujkkonstruksi/create.blade.php
@@ -13,6 +13,8 @@
 
       <!--begin::App Main-->
       <main class="app-main">
+        <section style="background-image: url('/assets/00_android/iconmenu/menuutama.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100%; min-height: 100vh;" loading="lazy">
+
         <!--begin::App Content Header-->
         <div class="app-content-header">
           <!--begin::Container-->
@@ -261,7 +263,9 @@
         <!--end::App Content Header-->
         <!--begin::App Content-->
           <!--end::App Content-->
-      </main>
+
+        </section>
+        </main>
       <!--end::App Main-->
     </div>
     </div>
