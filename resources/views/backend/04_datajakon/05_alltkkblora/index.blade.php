29e4eb733221b66f824606b5147542b861feda2e
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 00:30:56 +0700

    ergreg

--- resources/views/backend/04_datajakon/05_alltkkblora/index.blade.php
+++ resources/views/backend/04_datajakon/05_alltkkblora/index.blade.php
@@ -42,7 +42,8 @@
          <!--begin::Row-->
          <div class="row">
 
-           <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+            @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+            @include('backend.00_administrator.00_baganterpisah.11_alert')
 
          </div>
          <!--end::Row-->
@@ -53,12 +54,6 @@
      <!-- Menampilkan pesan sukses -->
 <br>
      {{-- ======================================================= --}}
-     {{-- ALERT --}}
-
-     @include('backend.00_administrator.00_baganterpisah.06_alert')
-
-     {{-- ======================================================= --}}
-
      <div class="container-fluid">
          <!--begin::Row-->
          <div class="row" style="margin-right: 10px; margin-left:10px;">
@@ -150,21 +145,8 @@
                  </div>
                  <!-- /.card-header -->
                  <div class="card-header">
-                    <div style="
-                    margin-bottom:10px;
-                    font-weight: 900;
-                    font-size: 16px;
-                    text-align: center;
-                    background: linear-gradient(135deg, #166534, #166534);
-                    color: white;
-                    padding: 10px 25px;
-                    border-radius: 10px;
-                    display: inline-block;
-                    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                    width: 100%;
-                ">
-                    📌 Halaman : {{$title}}
-                </div>
+                    @include('backend.00_administrator.00_baganterpisah.10_judulhalaman')
+
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
                         <div style="position: relative; display: inline-block; margin-right:10px;">
