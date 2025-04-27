97713a29e769c3fc5daabe19badb98ef6baa9740
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-27 21:15:17 +0700

    eg

--- resources/views/backend/04_datajakon/04_tkkdpupr/index.blade.php
+++ resources/views/backend/04_datajakon/04_tkkdpupr/index.blade.php
@@ -53,13 +53,6 @@
 
      <!-- Menampilkan pesan sukses -->
 <br>
-     {{-- ======================================================= --}}
-     {{-- ALERT --}}
-
-     @include('backend.00_administrator.00_baganterpisah.06_alert')
-
-     {{-- ======================================================= --}}
-
      <div class="container-fluid">
          <!--begin::Row-->
          <div class="row" style="margin-right: 10px; margin-left:10px;">
@@ -67,21 +60,7 @@
 
                  <!-- /.card-header -->
                  <div class="card-header">
-                    <div style="
-                    margin-bottom:10px;
-                    font-weight: 900;
-                    font-size: 16px;
-                    text-align: center;
-                    background: linear-gradient(135deg, #166534, #166534);
-                    color: white;
-                    padding: 10px 25px;
-                    border-radius: 10px;
-                    display: inline-block;
-                    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                    width: 100%;
-                ">
-                    📌 Halaman : {{$title}}
-                </div>
+                    @include('backend.00_administrator.00_baganterpisah.10_judulhalaman')
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
                         <div style="position: relative; display: inline-block; margin-right:10px;">
