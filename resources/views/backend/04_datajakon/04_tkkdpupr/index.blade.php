70bed6637d5274d238833f55312a5c9bab26e224
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-27 21:31:39 +0700

    eg

--- resources/views/backend/04_datajakon/04_tkkdpupr/index.blade.php
+++ resources/views/backend/04_datajakon/04_tkkdpupr/index.blade.php
@@ -248,7 +248,7 @@
              <a href="/beskkdpupr/show/{{$item->nama}}" class="btn btn-sm btn-info me-2" title="Show">
                  <i class="bi bi-eye"></i>
              </a>
-             <a href="/404" class="btn btn-sm btn-warning me-2" title="Update">
+             <a href="/beskkdpupr/update/{{$item->nama}}" class="btn btn-sm btn-warning me-2" title="Update">
                  <i class="bi bi-pencil-square"></i>
              </a>
              <a href="javascript:void(0)" class="btn btn-sm btn-danger" title="Delete"
