0195e0de1c9f9150bf5f90b4fadc56863f1aef1a
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 00:38:39 +0700

    eg

--- resources/views/backend/04_datajakon/05_alltkkblora/show.blade.php
+++ resources/views/backend/04_datajakon/05_alltkkblora/show.blade.php
@@ -22,8 +22,8 @@
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-
-              <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
 
             </div>
             <!--end::Row-->
