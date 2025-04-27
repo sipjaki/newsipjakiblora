d4ae77f704c7160d48cdc37a7cd3b4cf0d197eb8
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 02:12:39 +0700

    eg

--- resources/views/frontend/00_android/C_datajakon/index.blade.php
+++ resources/views/frontend/00_android/C_datajakon/index.blade.php
@@ -125,7 +125,35 @@
                                 </div>
 
                             <div class="flex flex-col gap-1">
-                        <p class="font-bold line-clamp-1 hover:line-clamp-none" style="color: black;">Data Statistika</p>
+                        <p class="font-bold line-clamp-1 hover:line-clamp-none" style="color: black;">Data Statistik BUJK</p>
+                        </p>
+
+                            </div>
+                            </div>
+                            </a>
+
+                            <a href="/404" class="card">
+                            <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
+                                <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
+                                    <img src="/assets/00_android/00_iconmenuutama/01_menuutam/statistika.png" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
+                                </div>
+
+                            <div class="flex flex-col gap-1">
+                        <p class="font-bold line-clamp-1 hover:line-clamp-none" style="color: black;">Data Statistik Tenaga Kerja</p>
+                        </p>
+
+                            </div>
+                            </div>
+                            </a>
+
+                            <a href="/404" class="card">
+                            <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
+                                <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
+                                    <img src="/assets/00_android/00_iconmenuutama/01_menuutam/statistika.png" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
+                                </div>
+
+                            <div class="flex flex-col gap-1">
+                        <p class="font-bold line-clamp-1 hover:line-clamp-none" style="color: black;">Data Statistik Profil Pekerjaan</p>
                         </p>
 
                             </div>
