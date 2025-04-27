02052c97cdb70054af3e93635d77169efad4a43f
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 02:47:36 +0700

    eg

--- resources/views/backend/04_datajakon/06_profilpaketpekerjaan/index.blade.php
+++ resources/views/backend/04_datajakon/06_profilpaketpekerjaan/index.blade.php
@@ -33,14 +33,18 @@
 
    <!--begin::App Main-->
    <main class="app-main">
-     <!--begin::App Content Header-->
+    <section style="background-image: url('/assets/00_android/iconmenu/menuutama.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100%; min-height: 100vh;" loading="lazy">
+
+
+    <!--begin::App Content Header-->
      <div class="app-content-header">
        <!--begin::Container-->
        <div class="container-fluid">
          <!--begin::Row-->
          <div class="row">
 
-           <div class="col-sm-12"><h3 class="mb-0">Selamat datang ! <span style="color: black; font-weight:800;" > {{ Auth::user()->name }}</span> di Dashboard <span style="color: black; font-weight:800;"> {{ Auth::user()->statusadmin->statusadmin }} </span>  Sistem Informasi Pembina Jasa Konstruksi Kab Blora</h3></div>
+            @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+            @include('backend.00_administrator.00_baganterpisah.11_alert')
 
          </div>
          <!--end::Row-->
@@ -50,12 +54,6 @@
 
      <!-- Menampilkan pesan sukses -->
 <br>
-     {{-- ======================================================= --}}
-     {{-- ALERT --}}
-
-     @include('backend.00_administrator.00_baganterpisah.06_alert')
-
-     {{-- ======================================================= --}}
 
      <div class="container-fluid">
          <!--begin::Row-->
@@ -138,21 +136,7 @@
                  </div>
                  <!-- /.card-header -->
                  <div class="card-header">
-                    <div style="
-                    margin-bottom:10px;
-                    font-weight: 900;
-                    font-size: 16px;
-                    text-align: center;
-                    background: linear-gradient(135deg, #166534, #166534);
-                    color: white;
-                    padding: 10px 25px;
-                    border-radius: 10px;
-                    display: inline-block;
-                    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                    width: 100%;
-                ">
-                    📌 Halaman : {{$title}}
-                </div>
+                    @include('backend.00_administrator.00_baganterpisah.10_judulhalaman')
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
                         <div style="position: relative; display: inline-block; margin-right:10px;">
@@ -384,7 +368,8 @@
      <!--end::App Content Header-->
      <!--begin::App Content-->
        <!--end::App Content-->
-   </main>
+            </section>
+    </main>
    <!--end::App Main-->
  </div>
  </div>
