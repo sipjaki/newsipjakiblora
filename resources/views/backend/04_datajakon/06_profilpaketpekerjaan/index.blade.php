24d8bad8e9713a5749cff8943e6cc5c763a62437
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 02:45:34 +0700

    egreg

--- resources/views/backend/04_datajakon/06_profilpaketpekerjaan/index.blade.php
+++ resources/views/backend/04_datajakon/06_profilpaketpekerjaan/index.blade.php
@@ -303,7 +303,7 @@
 
 
 
-<td style="text-align: left;">{{ ucfirst(strtolower($item->user->name)) }}</td>
+<td style="text-align: left;">{{ ucwords(strtolower($item->user->name)) }}</td>
 
          <td style="text-align: center; vertical-align: middle;">
              <a href="/bepaketpekerjaan/show/{{$item->namapekerjaan}}" class="btn btn-sm btn-info me-2" title="Show">
