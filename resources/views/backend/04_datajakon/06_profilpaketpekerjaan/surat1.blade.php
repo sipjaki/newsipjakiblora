88983c1b4dcb3fef86bd39c79a0454f1fe8ca56f
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 15:16:03 +0700

    eg

--- resources/views/backend/04_datajakon/06_profilpaketpekerjaan/surat1.blade.php
+++ resources/views/backend/04_datajakon/06_profilpaketpekerjaan/surat1.blade.php
@@ -212,6 +212,19 @@
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
 
+                            <a href="/bepekerjaandetailsupdate/{id}">
+                                <button
+                                onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                                onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
+                                style="background-color: #27c93c; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                                <!-- Ikon Kembali -->
+                                <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 384 512" fill="white" style="margin-right: 8px;">
+                                    <path d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zM384 121.9V128H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z"/>
+                                </svg>
+                                Update
+                            </button>
+                            </a>
+
                             <a href="/bepaketpekerjaan">
                                 <button
                                 onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
