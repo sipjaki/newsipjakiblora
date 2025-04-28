839c0bb90809d5dc710a042c01f5a7269f973e85
SipjakiBlora <sipjakiblora@gmail.com>
2025-04-28 14:33:49 +0700

    eg

--- resources/views/backend/04_datajakon/06_profilpaketpekerjaan/surat1.blade.php
+++ resources/views/backend/04_datajakon/06_profilpaketpekerjaan/surat1.blade.php
@@ -236,7 +236,7 @@
                             <div class="header-text">
                                 <h3>PEMERINTAH KABUPATEN BLORA</h3>
                                 <h3 style="text-transform: uppercase;">{{$data->user->name}}</h3>
-                                <p>{{$data->alamat}}</p>
+                                <p>{{$data->user->alamat}}</p>
                                 {{-- <h3>BLORA 58214</h3> --}}
                             </div>
                         </div>
