3b047e38fcf167a8751cc72ec916f2cfd4500504
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-03 17:46:42 +0700

    eg

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/index.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/index.blade.php
@@ -334,63 +334,63 @@
                               <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->sesuai_jenis == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-x-circle" style="margin-right: 8px;"></i>{{ $item->sesuai_jenis }}
+                                    <i class="bi {{ $item->sesuai_jenis == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->sesuai_jenis }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->sesuai_sifat == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-check-circle" style="margin-right: 8px;"></i>{{ $item->sesuai_sifat }}
+                                    <i class="bi {{ $item->sesuai_sifat == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->sesuai_sifat }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->sesuai_klasifikasi == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-file-earmark-text" style="margin-right: 8px;"></i>{{ $item->sesuai_klasifikasi }}
+                                    <i class="bi {{ $item->sesuai_klasifikasi == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->sesuai_klasifikasi }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->sesuai_layanan == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-gear" style="margin-right: 8px;"></i>{{ $item->sesuai_layanan }}
+                                    <i class="bi {{ $item->sesuai_layanan == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->sesuai_layanan }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->segmentasipasar_bentuk == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-pyramid" style="margin-right: 8px;"></i>{{ $item->segmentasipasar_bentuk }}
+                                    <i class="bi {{ $item->segmentasipasar_bentuk == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->segmentasipasar_bentuk }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->segmentasipasar_kualifikasi == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-bar-chart" style="margin-right: 8px;"></i>{{ $item->segmentasipasar_kualifikasi }}
+                                    <i class="bi {{ $item->segmentasipasar_kualifikasi == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->segmentasipasar_kualifikasi }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->syarat_SBU == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-file-earmark-check" style="margin-right: 8px;"></i>{{ $item->syarat_SBU }}
+                                    <i class="bi {{ $item->syarat_SBU == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->syarat_SBU }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->syarat_NIB == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-file-earmark-lock" style="margin-right: 8px;"></i>{{ $item->syarat_NIB }}
+                                    <i class="bi {{ $item->syarat_NIB == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->syarat_NIB }}
                                 </button>
                             </td>
 
                             <td style="text-align: center">
                                 <button style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer;
                                     background-color: {{ $item->pelaksanaanpengembangan == 'BELUM TERTIB' ? 'red' : 'blue' }};">
-                                    <i class="bi bi-clipboard" style="margin-right: 8px;"></i>{{ $item->pelaksanaanpengembangan }}
+                                    <i class="bi {{ $item->pelaksanaanpengembangan == 'BELUM TERTIB' ? 'bi-x-circle' : 'bi-check-circle' }}" style="margin-right: 8px;"></i>{{ $item->pelaksanaanpengembangan }}
                                 </button>
                             </td>
 
