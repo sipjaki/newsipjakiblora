d1c9eb5047a1a79b756e2e6190d1109d30fed479
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-05 02:04:13 +0700

    eg

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/01_surat1/create.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/01_surat1/create.blade.php
@@ -223,7 +223,7 @@
                                                             <option value="">-- Pilih Subklasifikasi --</option>
                                                             @foreach ($datasubklasifikasi as $sub)
                                                                 <option value="{{ $sub->id }}">
-                                                                    {{ \Illuminate\Support\Str::limit($sub->pekerjaan, 50) }}
+                                                                    {{ \Illuminate\Support\Str::limit($sub->pekerjaan, 200) }}
                                                                 </option>
                                                             @endforeach
                                                         </select>
