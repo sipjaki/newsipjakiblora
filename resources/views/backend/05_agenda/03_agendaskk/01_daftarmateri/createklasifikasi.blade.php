d2424717883f732f25ef3dc91f8ec4963fa3bfdc
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-06 00:02:43 +0700

    eg

--- resources/views/backend/05_agenda/03_agendaskk/01_daftarmateri/createklasifikasi.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/01_daftarmateri/createklasifikasi.blade.php
@@ -60,7 +60,7 @@
                                 </button>
 
                                 <!-- Tombol Kanan -->
-                                <a href="/beagendaskkmateri//{{$data->id}}">
+                                <a href="/beagendaskkmateri/{{$data->id}}">
                                     <button
                                         onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                                         onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
