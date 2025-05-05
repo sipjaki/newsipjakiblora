ebb2934b21a08b8a804c0f726ad8b9de5e43e284
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-06 00:58:43 +0700

    eg

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -153,7 +153,7 @@ table.zebra-table {
     <div class="full-width-container mx-auto px-4 pb-10">
         <div class="bg-white flex flex-col gap-5 p-5 rounded-[20px] shadow-md w-full">
             <div style="display: flex; justify-content: center; margin-bottom: -20px;">
-                <div style="width: 100%; max-width: 500px;"> <!-- Optional max-width buat tampilan elegan -->
+                <div style="width: 100%; max-width: 900px;"> <!-- Optional max-width buat tampilan elegan -->
                     <button type="button" style="
                     display: inline-flex;
                     flex-direction: row;
