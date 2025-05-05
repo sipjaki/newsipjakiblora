2f9aaa0edf267425bbd9aa5ec24dda83dbaf70b6
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-05 16:39:29 +0700

    eg

--- app/Models/surattertibjakonusaha1.php
+++ app/Models/surattertibjakonusaha1.php
@@ -15,7 +15,7 @@ class surattertibjakonusaha1 extends Model
 
     public function tertibjasakonstruksi()
     {
-        return $this->hasOne(tertibjasakonstruksi::class, 'tertibjasakonstruksi_id');
+        return $this->hasOne(surattertibjakonusaha1::class, 'tertibjasakonstruksi_id', 'id');
     }
 
     public function tandatangan1()
