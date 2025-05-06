75a871af34be1049fdec31406864a84d311f6b6e
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-06 11:56:55 +0700

    eg

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -363,7 +363,8 @@ table.zebra-table {
                                     <!-- Punya SKK -->
                                     <div class="col-md-4" style="{{ $divStyle }}">
                                         <label class="form-label" style="{{ $labelStyle }}">
-                                            <i class="bi bi-card-checklist" style="color: navy;"></i> Punya SKK?
+                                            <i class="bi bi-card-checklist" style="color: navy;"></i> Apakah sebelumnya sudah pernah memiliki SKK dengan jabatan kerja yang lain ?
+
                                         </label>
                                         <select name="punyaskk" style="{{ $inputStyle }}" class="@error('punyaskk') is-invalid @enderror">
                                             <option value="">-- Pilih --</option>
