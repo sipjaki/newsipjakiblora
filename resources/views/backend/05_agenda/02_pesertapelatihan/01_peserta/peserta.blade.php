8525fd8ffd3a6ad607e15f15a7471a7dc91fd42d
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-07 14:02:32 +0700

    eg

--- resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/peserta.blade.php
+++ resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/peserta.blade.php
@@ -142,7 +142,7 @@
                 4: { cellWidth: 28 },   // Tgl Lahir
                 5: { cellWidth: 28 },   // No Telp
                 6: { cellWidth: 30 },   // Instansi
-                7: { cellWidth: 30 },   // Sertifikat
+                // 7: { cellWidth: 30 },   // Sertifikat
                 // 8: { cellWidth: 25 },   // Verifikasi
                 // 9: { cellWidth: 30 }    // Upload
             },
