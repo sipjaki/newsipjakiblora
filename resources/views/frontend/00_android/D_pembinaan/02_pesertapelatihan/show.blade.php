a31c6329b824050e41cb5c8ab26d32f6b9406bb9
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-07 15:42:35 +0700

    eg

--- resources/views/frontend/00_android/D_pembinaan/02_pesertapelatihan/show.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/02_pesertapelatihan/show.blade.php
@@ -225,7 +225,7 @@ h5 {
                                             <th onclick="sortTable(1)" style="cursor:pointer; text-align:center; width:200px;"> Nama Lengkap  </th>
                                             <th onclick="sortTable(3)" style="cursor:pointer; text-align:center; width:300px;"> Gender </th>
                                             <th onclick="sortTable(8)" style="cursor:pointer; text-align:center; width:300px;"> Instansi </th>
-                                            <th onclick="sortTable(4)" style="cursor:pointer; text-align:center;">
+                                            <th onclick="sortTable(4)" style="cursor:pointer; text-align:center; width:300px;">
                                                 <i class="bi bi-check2-circle me-1"></i> Status
                                             </th>
                                             {{-- <th style="text-align:center; width:100px;"> View Peserta </th> --}}
