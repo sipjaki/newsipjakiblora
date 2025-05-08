eeee2a01b217bd9b8f0a5c5fc49622f5043028e8
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-08 09:20:30 +0700

    reg

--- resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
@@ -105,20 +105,18 @@
                 </button>
 
                 <button
-                    {{-- onclick="window.history.back();" --}}
-                    onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                    onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
-                    style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
+                onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
+                style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
 
-                    <!-- Ikon Kembali -->
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        viewBox="0 0 16 16" style="margin-right: 8px;">
-                        <path fill-rule="evenodd"
-                            d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
-                    </svg>
-                    {{$data->user->name}}
-                </button>
-
+                <!-- Ikon Arah Kiri -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    viewBox="0 0 16 16" style="margin-right: 8px;">
+                    <path fill-rule="evenodd"
+                        d="M11.854 1.146a.5.5 0 0 1 0 .708L5.707 8l6.147 6.146a.5.5 0 0 1-.708.708l-6.5-6.5a.5.5 0 0 1 0-.708l6.5-6.5a.5.5 0 0 1 .708 0z"/>
+                </svg>
+                {{ $data->user->name }}
+            </button>
 
                      {{-- <a href="/beagendapelatihanmateri/createmateri/{{$datapeserta->id}}">
                         <button
