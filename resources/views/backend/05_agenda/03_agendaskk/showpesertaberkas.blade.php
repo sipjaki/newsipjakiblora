ec082b27c51a416fc18f848691bf546f15c09a9f
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-08 09:08:02 +0700

    eg

--- resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
@@ -89,25 +89,23 @@
                         @include('backend.00_administrator.00_baganterpisah.14_judulshow')
                 <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
 
-                    <a href="/beagendapelatihan">
-                        <button
-                        onclick="window.history.back();"
-                        onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                        onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
-                        style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
+                    <button
+                    onclick="window.history.back();"
+                    onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+                    onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
+                    style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
 
-                        <!-- Ikon Kembali -->
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    <!-- Ikon Kembali -->
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                         viewBox="0 0 16 16" style="margin-right: 8px;">
                         <path fill-rule="evenodd"
-                        d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
+                            d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
                     </svg>
                     Kembali
                 </button>
-            </a>
 
 
-                     <a href="/beagendapelatihanmateri/createmateri/{{$data->id}}">
+                     {{-- <a href="/beagendapelatihanmateri/createmateri/{{$data->id}}">
                         <button
                          onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
                          onmouseout="this.style.backgroundColor='#166534'; this.style.color='white';"
@@ -116,7 +114,7 @@
                          <i class="fa fa-plus" style="margin-right: 8px;"></i>
                          Create
                      </button>
-                     </a>
+                     </a> --}}
                  </div>
 
                     </div>
@@ -243,7 +241,7 @@
                                         </td>
 
                                     </tr>
-                                        @endforeach
+                                        {{-- @endforeach --}}
 
                                 </tbody>
                             </table>
