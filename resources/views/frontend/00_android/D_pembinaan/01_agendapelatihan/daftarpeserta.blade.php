a03ccb1e1475fb0b8de6e28bc0fcae93385f7abb
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 06:27:10 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/01_agendapelatihan/daftarpeserta.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/01_agendapelatihan/daftarpeserta.blade.php
@@ -201,7 +201,7 @@ h5 {
                     <div class="header-surat">
                         <div class="header-text">
                             <h3>AGENDA PELATIHAN TKK KABUPATEN BLORA </h3>
-                            <h4>DINAS PEKERJAAN UMUM DAN PENATAAN RUANG <br> KABUPATEN BLORA PROVINSI JAWA TENGAH</h4>
+                            <h4>DPUPR KABUPATEN BLORA <br> PROVINSI JAWA TENGAH</h4>
                             <p>------------------------------------------------------------</p>
                             <p>FORMULIR PENDAFTARAN</p>
 
