5798820c048b2523e485c8121f7f0655634b74f4
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 02:20:33 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/01_agendapelatihan/show.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/01_agendapelatihan/show.blade.php
@@ -220,21 +220,22 @@ h5 {
                         </div>
                     </div>
 
-                    <div class="rounded-lg shadow-lg overflow-hidden w-fit">
-                        <div style="margin-top: 10px;">
-                            @if($data->foto && file_exists(public_path('storage/' . $data->foto)))
-                                <!-- Menampilkan gambar dari storage -->
-                                <img src="{{ asset('storage/' . $data->foto) }}" alt="Gambar Peraturan" style="width: 100%; max-height: 500px; object-fit: contain;" loading="lazy">
-                            @elseif($data->foto)
-                                <!-- Menampilkan gambar dari path luar storage -->
-                                <img src="{{ asset($data->foto) }}" alt="Gambar Peraturan" style="width: 100%; max-height: 500px; object-fit: contain;" loading="lazy">
-                            @else
-                                <!-- Placeholder jika tidak ada data -->
-                                <p>Data belum diupdate</p>
-                            @endif
-                        </div>
-
-                    </div>
+             <!-- Container tengah -->
+                            <div class="flex justify-center items-center min-h-screen">
+                                <div class="rounded-lg shadow-lg overflow-hidden w-fit">
+                                    <div style="margin-top: 10px;">
+                                        @if($data->foto && file_exists(public_path('storage/' . $data->foto)))
+                                            <img src="{{ asset('storage/' . $data->foto) }}" alt="Gambar Peraturan"
+                                                style="width: 100%; max-height: 500px; object-fit: contain;" loading="lazy">
+                                        @elseif($data->foto)
+                                            <img src="{{ asset($data->foto) }}" alt="Gambar Peraturan"
+                                                style="width: 100%; max-height: 500px; object-fit: contain;" loading="lazy">
+                                        @else
+                                            <p>Data belum diupdate</p>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
 
                     <br>
                     <h4 style="font-weight:bold;">I. INFORMASI AGENDA PELATIHAN </h4>
