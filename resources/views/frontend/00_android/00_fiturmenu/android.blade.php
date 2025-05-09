bdb2d685cb825f69598036267914b0777a67c61e
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 03:02:42 +0700

    e

--- resources/views/frontend/00_android/00_fiturmenu/android.blade.php
+++ resources/views/frontend/00_android/00_fiturmenu/android.blade.php
@@ -1,7 +1,7 @@
 <div id="Menu-bar" class="fixed bottom-[24px] px-[18px] max-w-[640px] w-full z-30" style="margin-bottom: -25px;">
 
     <div class="bg-white p-[14px_12px] rounded-full flex items-center justify-between gap-4 shadow-[0_8px_30px_0_#0A093212] w-full">
-        <a href="/404">
+        <a href="/dashboard">
             <div class="flex flex-col gap-1 items-center">
                 <div class="w-6 h-6 flex shrink-0">
                     <img src="/assets/00_android/menuhape/1.png" alt="icon" loading="lazy">
