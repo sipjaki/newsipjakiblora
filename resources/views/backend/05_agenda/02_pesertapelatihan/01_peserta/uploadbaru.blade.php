db24b01a4a282c5c58deeefe42b42cf8658b2891
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 22:31:06 +0700

    e

--- resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
+++ resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
@@ -11,6 +11,12 @@
             box-sizing: border-box;
         }
 
+        .cert-header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 20px;
+        }
     .cert-logos {
         display: flex;
         gap: 20px;
