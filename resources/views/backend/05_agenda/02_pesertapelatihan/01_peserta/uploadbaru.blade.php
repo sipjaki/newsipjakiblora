a2c5314dcbdf1d58134b423df79bb080ab2da16a
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 22:22:41 +0700

    e

---
+++ resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
@@ -0,0 +1,117 @@
+<style>
+    .readonly {
+    background-color: #f0f0f0 !important;
+    color: #6c757d;
+}
+</style>
+
+@include('backend.00_administrator.00_baganterpisah.01_header')
+
+<!--begin::Body-->
+  <body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+{{-- ---------------------------------------------------------------------- --}}
+
+@include('backend.00_administrator.00_baganterpisah.04_navbar')
+{{-- ---------------------------------------------------------------------- --}}
+
+      @include('backend.00_administrator.00_baganterpisah.03_sidebar')
+
+      <!--begin::App Main-->
+      <main class="app-main">
+
+        <section style="background-image: url('/assets/00_android/iconmenu/menuutama.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100%; min-height: 100vh;" loading="lazy">
+
+        <!--begin::App Content Header-->
+        <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+
+                @include('backend.00_administrator.00_baganterpisah.09_selamatdatang')
+                @include('backend.00_administrator.00_baganterpisah.11_alert')
+
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+
+        <br>
+        <!-- Menampilkan pesan sukses -->
+
+        {{-- ======================================================= --}}
+        {{-- ALERT --}}
+
+        @include('backend.00_administrator.00_baganterpisah.06_alert')
+
+        {{-- ======================================================= --}}
+
+            <!-- Menyertakan FontAwesome untuk ikon -->
+
+        <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row" style="margin-right: 10px; margin-left:10px;">
+                <!-- /.card -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        @include('backend.00_administrator.00_baganterpisah.12_judulupdate')
+
+
+        <div class="card card-primary card-outline mb-6">
+            <div style="display: flex; justify-content: flex-end; margin-top:10px;">
+                <button
+    onclick="history.back()"
+    onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
+    onmouseout="this.style.backgroundColor='#374151'; this.style.color='white';"
+    style="background-color: #374151; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s;">
+    <!-- Ikon Kembali -->
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+        viewBox="0 0 16 16" style="margin-right: 8px;">
+        <path fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z" />
+    </svg>
+    Kembali
+</button>
+
+        </div>
+        <hr>
+
+
+        {{-- ======================================================= --}}
+                    <div class="col-md-12">
+                        <!--begin::Quick Example-->
+
+
+
+
+                                                     </div>
+                        <!--end::Quick Example-->
+
+                    </div>
+
+                    <br><br>
+                    <!-- /.card -->
+                    <!-- Button Section -->
+
+                    </div>
+                    <!--end::Row-->
+                    </div>
+
+        </div>
+        <!--end::Row-->
+        </div>
+                  <!--end::Container-->
+        <!--end::App Content Header-->
+        <!--begin::App Content-->
+          <!--end::App Content-->
+        </section>
+        </main>
+      <!--end::App Main-->
+    </div>
+    </div>
+
+
+      @include('backend.00_administrator.00_baganterpisah.02_footer')
