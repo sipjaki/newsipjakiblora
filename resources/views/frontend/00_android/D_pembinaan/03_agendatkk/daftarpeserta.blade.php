6f7663b71dacfd591105cf261edcfc1fe245bbc5
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 20:35:13 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
@@ -247,7 +247,7 @@ h5 {
                                                     <div class="col-md-6">
                                                         @php
                                                         $inputStyle = "
-                                                            width: 400px;
+                                                            width: 350px;
                                                             padding: 10px 14px;
                                                             border-radius: 8px;
                                                             border: 1px solid #ccc;
