517b1d2322b3c595ea7adf8ce6c40d6947f0f6dc
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-11 01:32:14 +0700

    e

--- resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
+++ resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
@@ -433,6 +433,7 @@ clip-path: polygon(0 0, 100% 0, 80% 100%, 0% 100%);
 
                             </div>
                             <div class="ribbon-text">
+                                <p>{{$data->namalengkap}}</p>
                                 <p> Dinas Pekerjaan Umum Dan Penataan Ruang Kabupaten Blora
                                 <br> Diterbitkan Pada : {{ \Carbon\Carbon::parse($data->agendapelatihan->waktupelaksanaan)->locale('id')->isoFormat('D MMMM YYYY') }}
                                 </p>
