54ddfe49f8cef28a8754c5ab54fea5f138af711a
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 22:25:37 +0700

    e

--- resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
+++ resources/views/backend/05_agenda/02_pesertapelatihan/01_peserta/uploadbaru.blade.php
@@ -206,6 +206,9 @@
                         </div>
 
 
+                                                     </div>
+                        <!--end::Quick Example-->
+
                     </div>
 
                     <br><br>
