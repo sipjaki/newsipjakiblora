c6f83cb4ff37b4ec02891a0853c8b569147588b2
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-10 21:13:25 +0700

    e

--- resources/views/register/forgotpassword.blade.php
+++ resources/views/register/forgotpassword.blade.php
@@ -8,6 +8,7 @@
     <title>{{$title }}! | Sistem Informasi Pembina Jasa Konstruksi </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="icon" href="/assets/icon/logokabupatenblora.png" type="image/x-icon">
     <style>
 
 .toggle-password {
