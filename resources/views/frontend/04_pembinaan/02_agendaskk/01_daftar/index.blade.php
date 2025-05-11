84db0ef527b425ba236f0ea0f6aacffbe7887301
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 02:00:23 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -1472,6 +1472,7 @@ function handleSKKCheckbox(clickedBox) {
 
 </section>
 
+<br><br>
 
 <div class="progress-wrap">
           <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
