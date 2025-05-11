0bffbd13061fa5d555fbf125b4edca5f24211c1d
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 03:09:55 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -176,19 +176,19 @@ table.zebra-table {
         alert("{{ session('gagaldaftar') }}");
     </script>
 @endif
+<div style="width: 100%; background-color: #f0f0f0; padding: 10px; overflow: hidden;">
+    <p class="scrolling-text">Perhatian: Jika Anda kembali ke halaman ini setelah mendaftar, maka pendaftaran Anda gagal. Silakan periksa data dan dokumen Anda, lalu ulangi kembali !.</p>
+</div>
 
 <div id="sktContainer" style="display: flex; justify-content: center; align-items: center; margin-top:-60px;">
     <div id="sktBox" style="margin-bottom: 20px; font-family: 'Poppins', sans-serif; text-align: center; padding: 20px; border-radius: 15px; transition: background 0.3s ease; border: 1px solid #ddd;">
-        <div style="width: 100%; background-color: #f0f0f0; padding: 10px; overflow: hidden;">
-            <p class="scrolling-text">Perhatian: Jika Anda kembali ke halaman ini setelah mendaftar, maka pendaftaran Anda gagal. Silakan periksa data dan dokumen Anda, lalu ulangi kembali !.</p>
-        </div>
 
         <style>
           .scrolling-text {
             color: black;
             white-space: nowrap;
             display: inline-block;
-            animation: scrolling 10s linear infinite;
+            animation: scrolling 5s linear infinite;
             font-family: Arial, sans-serif;
             font-size: 16px;
           }
