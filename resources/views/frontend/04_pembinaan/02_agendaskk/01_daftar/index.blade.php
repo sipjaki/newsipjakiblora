17449a21c841041fbd62d930e9236d86f7b79f24
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 01:38:36 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -268,11 +268,16 @@ table.zebra-table {
 
     <!-- Kolom Kanan (Gambar) -->
     <div class="col-md-6" style="{{ $divStyle }}">
-        <div class="d-flex justify-content-end align-items-center" style="height: 100%; padding-right: 20px;">
-            <img src="/assets/icon/contoh1.png" width="25" height="25" alt="Blora" loading="lazy" style="margin-bottom: 5px;">
-            <img src="/assets/icon/contoh2.png" width="25" height="25" alt="PUPR" loading="lazy" style="margin-bottom: 5px;">
+        <p>Contoh SKK Tenaga Kerja Konstruksi </p>
+        <div class="d-flex justify-content-between align-items-center" style="height: 100%; padding-right: 20px;">
+            <!-- Gambar pertama -->
+            <img src="/assets/icon/contoh1.png" width="40" height="40" alt="Blora" loading="lazy" style="margin-bottom: 5px;">
+
+            <!-- Gambar kedua -->
+            <img src="/assets/icon/contoh2.png" width="40" height="40" alt="PUPR" loading="lazy" style="margin-bottom: 5px;">
         </div>
     </div>
+
 </div>
 
 <!-- Upload SKK -->
