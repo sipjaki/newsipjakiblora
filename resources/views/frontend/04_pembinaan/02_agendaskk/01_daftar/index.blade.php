fc5e7a9353fa3016278a3d07e8ee6f6c7345ba40
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 01:26:50 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -249,7 +249,7 @@ table.zebra-table {
                 $iconStyle = "color: navy;";
             @endphp
 
-<div class="col-md-6" style="{{ $divStyle }}">
+<div class="col-md-9" style="{{ $divStyle }}">
     <label class="form-label" style="{{ $labelStyle }}">
         <i class="bi bi-person-lines-fill" style="{{ $iconStyle }}"></i> Nama Jabatan Kerja SKK Anda
     </label>
