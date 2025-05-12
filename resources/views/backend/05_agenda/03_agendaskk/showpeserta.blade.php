f02a554eab5be5b0a22fb422de121bb82dd36d97
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 16:43:54 +0700

    e

--- resources/views/backend/05_agenda/03_agendaskk/showpeserta.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/showpeserta.blade.php
@@ -210,7 +210,7 @@
             <button type="submit" name="verifikasi" value="gugur" style="background-color: #EF4444; color: white; padding: 8px 16px; border-radius: 8px; border: none; cursor: pointer; font-weight: 500; display: inline-flex; align-items: center; transition: 0.3s;"
                 onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('i').style.color='black';"
                 onmouseout="this.style.backgroundColor='#EF4444'; this.style.color='white'; this.querySelector('i').style.color='white';">
-                <i class="bi bi-x-circle" style="margin-right: 6px;"></i> Gugur
+                <i class="bi bi-x-circle" style="margin-right: 6px;"></i> Dikembalikan
             </button>
         </form>
 
