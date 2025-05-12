5efc533d3ff19b41b6623379ced63216edc22b65
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 17:30:20 +0700

    e

--- resources/views/backend/05_agenda/03_agendaskk/index.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/index.blade.php
@@ -143,16 +143,19 @@ onmouseout="this.style.background='linear-gradient(135deg, #00378a, #FFD700)'; t
 
                                 </script>
 
-                         <a href="/beagendaskk/create">
-                             <button
-                             onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                             onmouseout="this.style.backgroundColor='#166534'; this.style.color='white';"
-                             style="background-color: #166534; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
-                             <!-- Ikon Kembali -->
-                             <i class="fa fa-plus" style="margin-right: 8px;"></i>
-                             Create
-                         </button>
-                         </a>
+                      <a href="/beagendaskk/create">
+    <button
+        onmouseover="this.style.background='white'; this.style.color='black'; this.style.border='1px solid #ccc';"
+        onmouseout="this.style.background='linear-gradient(135deg, #2e7d32, #d4af37)'; this.style.color='white'; this.style.border='none';"
+        style="background: linear-gradient(135deg, #2e7d32, #d4af37); color: white; border: none; margin-right: 10px; padding: 10px 20px;
+               border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center;
+               transition: background 0.3s, color 0.3s, border 0.3s;">
+        <!-- Ikon Plus -->
+        <i class="fa fa-plus" style="margin-right: 8px;"></i>
+        Create
+    </button>
+</a>
+
                      </div>
                  </div>
                  <!-- /.card-header -->
