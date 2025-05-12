8caa7c1dafe0def6255b50fdfaa2e40e9f1ffd42
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 22:36:52 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/04_navbar.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/04_navbar.blade.php
@@ -7,7 +7,7 @@
           <ul class="navbar-nav">
             <li class="nav-item">
   <a class="nav-link" data-lte-toggle="sidebar" href="#" role="button">
-    <i class="bi bi-list" style="color: black;"></i>
+    <i class="bi bi-list" style="color: black;">Menu</i>
   </a>
 </li>
 
