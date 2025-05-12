781ac09b6996848de9ff9539ff2b3c66e9e8161e
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 18:30:40 +0700

    e

--- resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
@@ -222,6 +222,7 @@
         <h5 style="text-align: center;">DATA PESERTA SKK TENAGA KONSTRUKSI</h5>
         <h5 style="text-align: center;">DINAS PEKERJAAN UMUM DAN PENATAAN RUANG KABUPATEN BLORA</h5>
     </div>
+    </div>
 
     <br>
 
@@ -625,6 +626,7 @@
 
     </table>
 
+    </div>
     </div>
 
     <div style="display: flex; justify-content: flex-end; padding: 10px;">
