55cb43debb29a26603be6dc91d3c8ff47c0fb1d5
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-12 17:27:47 +0700

    e

--- resources/views/backend/05_agenda/03_agendaskk/index.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/index.blade.php
@@ -60,23 +60,23 @@
              <!-- /.card -->
              <div class="card mb-4">
                  <div class="card-header">
-                    <div style="
-                    font-weight: 900;
-                    font-size: 16px;
-                    text-align: center;
-                    background: linear-gradient(135deg, #00378a, #00378a);
-                    color: white;
-                    padding: 8px 16px;
-                    border-radius: 10px;
-                    display: inline-block;
-                    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
-                    transition: all 0.3s ease;
-                    cursor: pointer;
-                "
-                onmouseover="this.style.background='white'; this.style.color='black';"
-                onmouseout="this.style.background='linear-gradient(135deg, #00378a, #00378a)'; this.style.color='white';">
-                    ⚙️ Setting Database
-                </div>
+<div style="
+    font-weight: 900;
+    font-size: 16px;
+    text-align: center;
+    background: linear-gradient(135deg, #00378a, #FFD700);
+    color: white;
+    padding: 8px 16px;
+    border-radius: 10px;
+    display: inline-block;
+    box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+    transition: all 0.3s ease;
+    cursor: pointer;
+"
+onmouseover="this.style.background='white'; this.style.color='black';"
+onmouseout="this.style.background='linear-gradient(135deg, #00378a, #FFD700)'; this.style.color='white';">
+    ⚙️ Setting Database
+</div>
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
 <!-- Tombol Data Penyelenggara/Asosiasi -->
