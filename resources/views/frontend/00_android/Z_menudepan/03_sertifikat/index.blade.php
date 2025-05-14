ef21292b888e262fe6fe60a1e2ad9731250733ed
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-14 22:44:50 +0700

    e

--- resources/views/frontend/00_android/Z_menudepan/03_sertifikat/index.blade.php
+++ resources/views/frontend/00_android/Z_menudepan/03_sertifikat/index.blade.php
@@ -37,7 +37,7 @@
                         <a href="/sertifikatpelatihan" class="card">
                             <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
                                 <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
-                                    <img src="/assets/00_android/00_iconmenuutama/01_menuutam/divisi.png" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
+                                    <img src="/assets/00_android/00_iconmenuutama/01_menuutam/sertifikatpelatihan.avif" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
                                 </div>
 
                             <div class="flex flex-col gap-1">
@@ -52,7 +52,7 @@
                         <a href="/404" class="card">
                             <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
                                 <div class="w-16 h-[60px] flex shrink-0 rounded-2xl overflow-hidden">
-                                    <img src="/assets/00_android/00_iconmenuutama/01_menuutam/divisi.png" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
+                                    <img src="/assets/00_android/00_iconmenuutama/01_menuutam/sertifikattkk.avif" class="w-full h-full object-cover" alt="thumbnail" loading="lazy">
                                 </div>
 
                             <div class="flex flex-col gap-1">
