e0488acfc56e4981b467ac2de1eede88a7ee578e
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-15 02:41:24 +0700

    e

--- resources/views/frontend/00_android/Z_menudepan/03_sertifikat/terbitpelatihan.blade.php
+++ resources/views/frontend/00_android/Z_menudepan/03_sertifikat/terbitpelatihan.blade.php
@@ -296,7 +296,7 @@
                             <div class="w-full border border-[#E8E9EE] flex items-center p-[14px] gap-3 rounded-2xl bg-white">
 
                 <div class="col-md-12" style="height:400px;">
-                 <div style="display: flex; justify-content: flex-start; width: 100%; overflow: auto; margin-left:-45px;">
+                 <div style="display: flex; justify-content: flex-start; width: 100%; overflow: auto; margin-left:-40px;">
                     <div style="transform: scale(0.3); transform-origin: top left;">
                         <div class="cert-container">
                             <div class="cert-header" style="text-align: center;">
