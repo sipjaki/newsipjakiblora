eb5cb03f2a641103aa5f52bca0f38fbcb41b8d24
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-15 02:09:48 +0700

    e

--- resources/views/frontend/00_android/Z_menudepan/03_sertifikat/terbitpelatihan.blade.php
+++ resources/views/frontend/00_android/Z_menudepan/03_sertifikat/terbitpelatihan.blade.php
@@ -256,38 +256,41 @@
 
 
 @include('frontend.00_android.00_fiturmenu.header')
+
 <body class="font-poppins text-[#292E4B] bg-[#F6F9FC]">
-    <section class="certificate-container">
-        <div class="header">
-            <div class="header-content">
-                <nav class="flex justify-between items-center">
-                    <!-- Logo kiri -->
+    <section class="max-w-[640px] w-full min-h-screen mx-auto flex flex-col bg-[#FCF7F1] overflow-x-hidden pb-4">
+        {{-- <div class="header flex flex-col" style="background: linear-gradient(to bottom, #28A745, #FFD100); border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden;"> --}}
+            <div class="header flex flex-col" style="background-image: url('/assets/00_android/iconmenu/menuutama1.jpg'); background-size: cover; background-position: center; border-bottom-left-radius: 50px; border-bottom-right-radius: 50px; overflow: hidden; height: 350px;">
+                <nav class="pt-5 px-3 flex justify-between items-center">
+                    <!-- Logo bagian kiri -->
                     <div class="flex items-center gap-[10px]">
                         <a href="index.html" class="w-10 h-10 flex shrink-0">
                             <img src="/assets/icon/logokabupatenblora.png" alt="icon" loading="lazy" class="w-full h-full object-contain">
                         </a>
                     </div>
 
-                    <!-- Judul tengah -->
+                    <!-- Judul bagian tengah -->
                     <div class="flex flex-col items-center justify-center text-center">
                         <p class="font-semibold text-xl text-white">Dinas Pekerjaan Umum Dan Penataan Ruang <br> Kabupaten Blora</p>
+                        <br>
+                        {{-- <p class="font-semibold text-xl text-white">{{$title}}</p> --}}
+                      <div class="z-10" style="margin-top:25px; background-color: rgba(255, 255, 255, 0.7); border-radius: 10px; padding: 20px; max-width: 600px; margin-left: auto; margin-right: auto;">
+            <h1 class="font-bold leading-[36px] text-center" style="color: black; font-size:20px;">
+                {{$title}}
+            </h1>
+        </div>
                     </div>
 
-                    <!-- Logo kanan -->
+                    <!-- Ikon bagian kanan -->
                     <a href="" class="w-10 h-10 flex shrink-0">
                         <img src="/assets/icon/pupr.png" alt="icon" loading="lazy" class="w-full h-full object-contain">
+
                     </a>
                 </nav>
-
-                <div class="title-box">
-                    <h1 class="font-bold leading-[36px] text-center" style="color: black; font-size:20px;">
-                        {{$title}}
-                    </h1>
-                </div>
             </div>
-        </div>
 
-        <div class="flex flex-col gap-4 px-4" style="margin-top:-100px;">
+
+            <section class="certificate-container">
             <!-- Certificate Content 1 -->
             <div id="sertifikat-content-1" class="cert-card">
                 <div class="cert-container">
