cc1af189229a7ac70d8c41cb2d035bef531c4909
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 04:15:13 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -1311,7 +1311,7 @@ function previewFile(previewId, input) {
 <!-- Upload NPWP -->
 <div class="col-md-4" style="{{ $divStyle }}">
     <label class="form-label text-start" style="{{ $labelStyle }}">
-        <i class="bi bi-file-earmark" style="color: navy;"></i> Upload NPWP | .pdf | Max 5MB
+        <i class="bi bi-file-earmark" style="color: navy;"></i> Upload NPWP | jpg,jpeg,png | Max 5MB
     </label>
     <input type="file" name="uploadnpwp" style="{{ $inputStyle }}" class="form-control @error('uploadnpwp') is-invalid @enderror" accept="application/pdf,image/*" onchange="previewFile('npwpPreview', this)">
     <div class="invalid-feedback">@error('uploadnpwp') {{ $message }} @enderror</div>
