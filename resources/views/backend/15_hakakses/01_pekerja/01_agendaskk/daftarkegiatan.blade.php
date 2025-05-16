277d4c163ea1c1467ef574489259615876b6a071
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 00:50:21 +0700

    e

--- resources/views/backend/15_hakakses/01_pekerja/01_agendaskk/daftarkegiatan.blade.php
+++ resources/views/backend/15_hakakses/01_pekerja/01_agendaskk/daftarkegiatan.blade.php
@@ -94,20 +94,6 @@ onmouseout="this.style.background='linear-gradient(135deg, #00378a, #FFD700)'; t
 </a> --}}
 
 <!-- Tombol Data LSP Pelaksana -->
-
-<a href="/404">
-    <button
-        onmouseover="this.style.background='white'; this.style.color='black'; this.querySelector('svg').style.fill='black';"
-        onmouseout="this.style.background='linear-gradient(to right, #004d00, #000000)'; this.style.color='white'; this.querySelector('svg').style.fill='white';"
-        style="background: linear-gradient(to right, #004d00, #000000); color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: all 0.3s ease;">
-
-        <svg xmlns="http://www.w3.org/2000/svg" style="margin-right: 8px; fill: white; transition: fill 0.3s;" width="20" height="20" viewBox="0 0 24 24">
-            <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 2.07 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
-        </svg>
-        Data LSP Pelaksana
-    </button>
-</a>
-
                      </div>
                  </div>
                  <!-- /.card-header -->
@@ -117,7 +103,7 @@ onmouseout="this.style.background='linear-gradient(135deg, #00378a, #FFD700)'; t
 
                      <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
                         <div style="position: relative; display: inline-block; margin-right:10px;">
-                            <input type="search" id="searchInput" placeholder="Cari Agenda Sertifikasi ...." onkeyup="searchTable()" style="border: 1px solid #ccc; padding: 10px 20px; font-size: 14px; border-radius: 10px; width: 300px;">
+                            <input type="search" id="searchInput" placeholder="Cari Agenda Sertifikasi Saudara...." onkeyup="searchTable()" style="border: 1px solid #ccc; padding: 10px 20px; font-size: 14px; border-radius: 10px; width: 300px;">
                             <i class="bi bi-search" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); font-size: 16px; color: #888;"></i>
                         </div>
                         <script>
