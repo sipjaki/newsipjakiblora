ae47686751fb5723cc75a124cb2a87144d90a221
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-16 17:06:04 +0700

    w

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -942,13 +942,13 @@ function previewFile(previewId, input) {
   </div>
 
   <!-- Preview di kanan (6 kolom) -->
-  <div class="col-md-6" id="preview_pdf" style="display: none; margin-top: 24px; margin-bottom: 30px;">
+  <div class="col-md-6" id="preview_pdf" style="display: none; margin-top: 24px; margin-bottom: 300px;">
   <label style="font-weight: bold;">Preview SKK:</label>
   <iframe
     id="pdf_preview_frame"
     width="100%"
     height="400px"
-    style="border: 1px solid #ccc; border-radius: 6px; margin-bottom: 30px;">
+    style="border: 1px solid #ccc; border-radius: 6px; margin-bottom: 300px;">
   </iframe>
 </div>
 
