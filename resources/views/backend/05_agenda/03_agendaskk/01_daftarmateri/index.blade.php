71d2268590721e13b476b20d6cb79118c7f00e85
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 01:08:52 +0700

    e

--- resources/views/backend/05_agenda/03_agendaskk/01_daftarmateri/index.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/01_daftarmateri/index.blade.php
@@ -158,7 +158,7 @@
                                 </thead>
                                 <tbody>
 
-                                @forelse ($subData as $item)
+                                @forelse ($subdata as $item)
                                     <tr class="align-middle">
                                         <td style="text-align: center;">{{ $loop->iteration }}</td>
                                         <td style="text-align: left;">{{ $item->judulskk }}</td>
