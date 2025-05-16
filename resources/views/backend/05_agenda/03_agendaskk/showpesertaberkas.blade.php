5a35bbfa1d5d42f936bc488b72da99967403e20e
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 04:12:45 +0700

    e

--- resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
+++ resources/views/backend/05_agenda/03_agendaskk/showpesertaberkas.blade.php
@@ -801,7 +801,7 @@
                                     <tr>
                                         <th style="width: 400px; text-align:left; font-size: 16px; background-color: green; color: white;"><i class="bi bi-file-earmark-text-fill"></i> Pas Foto 3 x 4 </th>
                                         <th class="text-center">
-                                            <button class="btn btn-secondary btn-sm"
+                                            <button type="button" class="btn btn-secondary btn-sm"
                                                 style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
                                                 onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                                                 onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';"
@@ -853,7 +853,7 @@
                                     <tr>
                                         <th style="width: 400px; text-align:left; font-size: 16px; background-color: green; color: white;"><i class="bi bi-file-earmark-text-fill"></i> Ijazah  </th>
                                         <th class="text-center">
-                                                <button class="btn btn-secondary btn-sm"
+                                                <button type="button" class="btn btn-secondary btn-sm"
                                                     style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
                                                     onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                                                     onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';"
@@ -909,7 +909,7 @@
                                     <tr>
                                         <th style="width: 400px; text-align:left; font-size: 16px; background-color: green; color: white;"><i class="bi bi-file-earmark-text-fill"></i> Pengalaman  </th>
                                        <th class="text-center align-middle" style="display: flex; justify-content: center; align-items: center;">
-                                            <button class="btn btn-secondary btn-sm"
+                                            <button type="button" class="btn btn-secondary btn-sm"
                                                 style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
                                                 onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                                                 onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';"
@@ -965,7 +965,7 @@
                                     <tr>
                                         <th style="width: 400px; text-align:left; font-size: 16px; background-color: green; color: white;"><i class="bi bi-file-earmark-text-fill"></i> Surat Kebenaran Data  </th>
                                         <th class="text-center align-middle" style="display: flex; justify-content: center; align-items: center;">
-                                                <button class="btn btn-secondary btn-sm"
+                                                <button type="button" class="btn btn-secondary btn-sm"
                                                     style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
                                                     onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                                                     onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';"
@@ -1022,7 +1022,7 @@
                                     <tr>
                                         <th style="width: 400px; text-align:left; font-size: 16px; background-color: green; color: white;"><i class="bi bi-file-earmark-text-fill"></i> NPWP </th>
                                         <th class="text-center align-middle" style="display: flex; justify-content: center; align-items: center;">
-                                                    <button class="btn btn-secondary btn-sm"
+                                                    <button type="button" class="btn btn-secondary btn-sm"
                                                         style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
                                                         onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                                                         onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';"
@@ -1079,7 +1079,7 @@
                                     <tr>
                                         <th style="width: 400px; text-align:left; font-size: 16px; background-color: green; color: white;"><i class="bi bi-file-earmark-text-fill"></i> Riwayat Hidup </th>
                                             <th class="text-center align-middle" style="display: flex; justify-content: center; align-items: center;">
-                                                <button class="btn btn-secondary btn-sm"
+                                                <button type="button" class="btn btn-secondary btn-sm"
                                                     style="border-radius: 15px; padding: 8px 16px; background-color: #6c757d; color: white; border: none; transition: background-color 0.3s, color 0.3s;"
                                                     onmouseover="this.style.backgroundColor='#ffffff'; this.style.color='#6c757d'; this.style.border='1px solid #6c757d';"
                                                     onmouseout="this.style.backgroundColor='#6c757d'; this.style.color='white'; this.style.border='none';"
