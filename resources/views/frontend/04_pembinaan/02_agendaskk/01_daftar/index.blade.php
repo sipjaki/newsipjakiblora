fc70544f399f6e4002777696c8c8663c9ad29f0d
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 00:19:26 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -928,8 +928,8 @@ function previewFile(previewId, input) {
             <form id="nikRegistrationForm" action="{{ route('daftarpesertasertifikasiskkcreatenew2') }}" method="POST" enctype="multipart/form-data" style="font-family: 'Poppins', sans-serif;">
                 @csrf
                 <input type="hidden" name="agendaskk_id" value="{{ $agendaskk_id }}">
-<div class="col-md-6" style="{{ $divStyle }} display: flex; gap: 20px;" id="upload_skk_section">
-    <!-- Bagian Upload kiri -->
+
+                {{-- <div class="col-md-6" style="{{ $divStyle }} display: flex; gap: 20px;" id="upload_skk_section">
     <div style="flex: 1; min-width: 0;">
         <label class="form-label" style="{{ $labelStyle }}">
             <i class="bi bi-upload" style="{{ $iconStyle }}"></i> Upload SKK Anda
@@ -938,9 +938,8 @@ function previewFile(previewId, input) {
         accept="application/pdf"
         style="{{ $inputStyle }}"
         class="form-control @error('skkanda') is-invalid @enderror">
-        {{-- <small class="form-text text-muted" style="font-size: 12px;">Format: PDF, maksimal 5MB</small> --}}
         @error('skkanda') <div class="invalid-feedback">{{ $message }}</div> @enderror
-    </div>
+    </div> --}}
 
     <!-- Bagian Preview kanan -->
 </div>
