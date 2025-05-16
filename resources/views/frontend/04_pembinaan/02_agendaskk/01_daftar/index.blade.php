a1819abefbfdac400691da31a6f7c50d5dc43f77
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-16 17:16:37 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -967,6 +967,7 @@ function previewFile(previewId, input) {
     }
   });
 </script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <div class="card-body">
 
