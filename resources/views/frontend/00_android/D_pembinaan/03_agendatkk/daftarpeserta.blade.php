b2278bfef975a59dc02d1dccc77bfca90883be4b
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 14:55:33 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
@@ -249,7 +249,7 @@ h5 {
 <br><br><br>
 
 <div id="sktContainer" style="display: flex; justify-content: center; align-items: center; margin-top:-60px;" width="200px">
-    <div id="sktBox" style="margin-bottom: 20px; font-family: 'Poppins', sans-serif; text-align: center; padding: 20px; border-radius: 15px; transition: background 0.3s ease; border: 1px solid #ddd;">
+    <div id="sktBox" style="margin-bottom: 20px; font-family: 'Poppins', sans-serif; text-align: center; padding: 20px; border-radius: 15px; transition: background 0.3s ease; border: 1px solid #ddd; width:400px;">
       <div style="width: 100%; background-color: #f0f0f0; padding: 10px; overflow: hidden;">
     @auth
         <div style="color: green; font-weight: bold; margin-bottom: 5px;">
