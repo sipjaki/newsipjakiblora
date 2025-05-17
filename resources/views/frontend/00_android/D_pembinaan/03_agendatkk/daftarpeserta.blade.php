f6bb715d8a62b795e31d9ba4063551aeb22e28ff
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 14:44:05 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
@@ -352,7 +352,7 @@ h5 {
                 @csrf
                 <input type="hidden" name="agendaskk_id" value="{{ $agendaskk_id }}">
 
-                <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center; gap: 5px;">
+                <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center; gap: 5px; margin-left:400px;" >
                     <hr style="width: 100%; max-width: 1200px; margin: 0;">
                     <p style="margin: 5px 0; color: black; white-space: nowrap; overflow: hidden; max-width: 800px;">
                         <span style="display: inline-block; animation: scroll-text 10s linear infinite;">
