4b78347f82dea89f46b3d4dc583c35852a591b84
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-17 15:18:51 +0700

    e

--- resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
+++ resources/views/frontend/00_android/D_pembinaan/03_agendatkk/daftarpeserta.blade.php
@@ -577,7 +577,7 @@ h5 {
         <i class="bi bi-person-workspace" style="{{ $iconStyle }}"></i> Jabatan Kerja
     </label>
     <p class="form-control-plaintext" style="{{ $inputStyle }}">
-        {{ $agendaskkjabatankerja->jabatankerja->jabatankerja }}
+        {{ $agendaskkjabatankerja }}
     </p>
     <input type="hidden" name="jabatankerja_id" value="{{ $agendaskkjabatankerja->jabatankerja->id }}">
 </div>
