1b8793dab9b26b1f864cc4b02a1298a3efc5b801
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-18 14:16:50 +0700

    e

--- resources/views/backend/16_settingsdata/01_sekolah/index.blade.php
+++ resources/views/backend/16_settingsdata/01_sekolah/index.blade.php
@@ -80,18 +80,20 @@
                 <div style="display: flex; justify-content: flex-end; margin-bottom: 5px;">
                  <a href="/beagendaskk">
                         <button
-                            onmouseover="this.style.backgroundColor='white'; this.style.color='black'; this.querySelector('svg').style.fill='black';"
-                            onmouseout="this.style.backgroundColor='#00378a'; this.style.color='white'; this.querySelector('svg').style.fill='white';"
-                            style="background-color: #00378a; color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background-color 0.3s, color 0.3s; text-decoration: none;">
+                            onmouseover="this.style.background='white'; this.style.color='black'; this.querySelector('svg').style.fill='black';"
+                            onmouseout="this.style.background='linear-gradient(to right, #004d00, #000000)'; this.style.color='white'; this.querySelector('svg').style.fill='white';"
+                            style="background: linear-gradient(to right, #004d00, #000000); color: white; border: none; margin-right: 10px; padding: 10px 20px; border-radius: 15px; font-size: 16px; cursor: pointer; display: flex; align-items: center; transition: background 0.3s, color 0.3s; text-decoration: none;">
 
                             <!-- Ikon Calendar SVG -->
-                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24" style="margin-right: 8px;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24" style="margin-right: 8px; transition: fill 0.3s;">
                                 <path d="M19 4h-1V2h-2v2H8V2H6v2H5c-1.1 0-1.99.9-1.99 2L3 20c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 16H5V10h14v10zm0-12H5V6h14v2z"/>
                             </svg>
 
                             Agenda SKK
                         </button>
                     </a>
+
+
                 </div>
 
                  </div>
