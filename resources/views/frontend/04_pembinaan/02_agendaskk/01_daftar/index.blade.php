37f16478e61e9b153e34c52e459158c2d63a1fbf
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-19 15:46:44 +0700

    e

--- resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
+++ resources/views/frontend/04_pembinaan/02_agendaskk/01_daftar/index.blade.php
@@ -799,7 +799,7 @@ table.zebra-table {
 <div class="col-md-4" style="{{ $divStyle }}">
     <div style="margin-top: 10px;">
     <iframe
-        src="https://docs.google.com/gview?url=https://yourdomain.com/assets/00_contohsurat/02_BUKTI_KEBENARAN_DATANEW.docx&embedded=true"
+        src="/assets/00_contohsurat/02_BUKTI_KEBENARAN_DATANEW.docx"
         style="width:100%; height:500px;"
         frameborder="0">
     </iframe>
