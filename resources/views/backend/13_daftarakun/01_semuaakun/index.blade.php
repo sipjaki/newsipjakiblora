22047db5994aac538c8434661f452cdade483f06
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-20 18:00:31 +0700

    e

--- resources/views/backend/13_daftarakun/01_semuaakun/index.blade.php
+++ resources/views/backend/13_daftarakun/01_semuaakun/index.blade.php
@@ -135,7 +135,7 @@
     {{$jumlahStatus3}} Pekerja
   </button>
 
-  <button class="btn-custom superadmin">
+  {{-- <button class="btn-custom superadmin">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
       class="bi bi-person-workspace" viewBox="0 0 16 16">
       <path d="M6 6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
@@ -144,7 +144,7 @@
         d="M12.5 11a.5.5 0 0 0 0 1H13v1a1 1 0 0 0 1 1h.5a.5.5 0 0 0 0-1H14v-1a1 1 0 0 0-1-1h-.5z" />
     </svg>
     {{$jumlahStatus1}} Super Admin
-  </button>
+  </button> --}}
 
   <button class="btn-custom admin">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
