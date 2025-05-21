322a7386641306de784bdee1b32d1588820c81bc
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-21 23:12:48 +0700

    eerg

--- app/Models/pengawasanbujk.php
+++ app/Models/pengawasanbujk.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class pengawasanbujk extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasApiTokens;
+
+    protected $guarded = ['id'];
+
 }
