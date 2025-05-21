baef3d2f73721358286655bb884f64e9102a37e0
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-22 00:17:20 +0700

    e

--- resources/views/backend/00_administrator/00_baganterpisah/01_header.blade.php
+++ resources/views/backend/00_administrator/00_baganterpisah/01_header.blade.php
@@ -67,6 +67,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="{{ asset('00_style/01_cssdashboard/style.css') }}">
 
     <style>
         * {
