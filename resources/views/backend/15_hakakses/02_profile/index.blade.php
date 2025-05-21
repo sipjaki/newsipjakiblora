73d3463e3509f28b655eee7fea3c687c15e2fbc5
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-21 12:36:29 +0700

    e

--- resources/views/backend/15_hakakses/02_profile/index.blade.php
+++ resources/views/backend/15_hakakses/02_profile/index.blade.php
@@ -329,7 +329,7 @@
                     </div>
                  </div>
 
-                 @include('backend.00_administrator.00_baganterpisah.07_paginations')
+                 {{-- @include('backend.00_administrator.00_baganterpisah.07_paginations') --}}
 
                  <br><br>
 
