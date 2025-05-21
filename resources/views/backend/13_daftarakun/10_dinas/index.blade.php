ec368f6bf290e76a9e826811b10d0594e43d7159
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-21 21:47:51 +0700

    e

--- resources/views/backend/13_daftarakun/10_dinas/index.blade.php
+++ resources/views/backend/13_daftarakun/10_dinas/index.blade.php
@@ -169,7 +169,7 @@
                             function searchTable() {
                             let input = document.getElementById("searchInput").value;
 
-                            fetch(`/alllsppenerbit?search=${input}`)
+                            fetch(`/alldinas?search=${input}`)
                                 .then(response => response.text())
                                 .then(html => {
                                     let parser = new DOMParser();
