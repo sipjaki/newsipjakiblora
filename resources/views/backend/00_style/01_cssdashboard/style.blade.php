5a780509199e1ec5a693666c33955d7adbad4425
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-22 15:00:21 +0700

    e

--- resources/views/backend/00_style/01_cssdashboard/style.blade.php
+++ resources/views/backend/00_style/01_cssdashboard/style.blade.php
@@ -8,4 +8,34 @@
     }
 }
 
+/* button create */
+
+.btn-create {
+  background: linear-gradient(45deg, #228B22, #d4af37);
+  color: white;
+  border: none;
+  margin-right: 10px;
+  padding: 10px 20px;
+  border-radius: 15px;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  gap: 8px;
+  transition: all 0.3s ease;
+  text-decoration: none;
+}
+
+.btn-create:hover {
+  background: white;
+  color: black;
+  transform: scale(1.05);
+}
+
+.icon-create {
+  font-size: 18px;
+}
+
+
 </style>
