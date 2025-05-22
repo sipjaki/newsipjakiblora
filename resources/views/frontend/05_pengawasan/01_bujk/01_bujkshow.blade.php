4ada4da92101bc64ad6fac813ba53353ce0a18c9
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-22 14:44:41 +0700

    e

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
@@ -558,7 +558,7 @@ table.zebra-table {
 
             </div>
 
-            @include('frontend.00_approve.01_cssterpisah.paginations')
+            {{-- @include('frontend.00_approve.01_cssterpisah.paginations') --}}
         </div>
     </div>
     <br><br><br>
