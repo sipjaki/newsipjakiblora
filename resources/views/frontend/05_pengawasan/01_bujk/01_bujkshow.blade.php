c176e8388e8c211e729a361283b255f44f31449f
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-22 14:50:10 +0700

    e

--- resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
+++ resources/views/frontend/05_pengawasan/01_bujk/01_bujkshow.blade.php
@@ -187,33 +187,37 @@ table.zebra-table {
     <thead>
 
 
-                   <a href="{{ url()->previous() }}">
-    <button
-  onmouseover="this.style.background='white'; this.style.color='black'; this.style.transform='scale(1.05)'"
-  onmouseout="this.style.background='linear-gradient(45deg, #6c757d, #adb5bd)'; this.style.color='white'; this.style.transform='scale(1)'"
-  style="
-    background: linear-gradient(45deg, #6c757d, #adb5bd);
-    color: white;
-    border: none;
-    margin-right: 10px;
-    padding: 10px 20px;
-    border-radius: 10px;
-    font-size: 16px;
-    font-weight: bold;
-    cursor: pointer;
-    display: inline-flex;
-    align-items: center;
-    gap: 8px;
-    transition: all 0.3s ease;
-  "
->
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-      viewBox="0 0 16 16">
-      <path fill-rule="evenodd"
-          d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z"/>
-  </svg>
-  Kembali
-</button>
+    <div class="d-flex justify-content-end">
+    <a href="{{ url()->previous() }}">
+        <button
+            onmouseover="this.style.background='white'; this.style.color='black'; this.style.transform='scale(1.05)'"
+            onmouseout="this.style.background='linear-gradient(45deg, #6c757d, #adb5bd)'; this.style.color='white'; this.style.transform='scale(1)'"
+            style="
+                background: linear-gradient(45deg, #6c757d, #adb5bd);
+                color: white;
+                border: none;
+                margin: 10px;
+                padding: 10px 20px;
+                border-radius: 10px;
+                font-size: 16px;
+                font-weight: bold;
+                cursor: pointer;
+                display: inline-flex;
+                align-items: center;
+                gap: 8px;
+                transition: all 0.3s ease;
+            "
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                    d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 7.5H14.5A.5.5 0 0 1 15 8z" />
+            </svg>
+            Kembali
+        </button>
+    </a>
+</div>
+
 
         <tr>
             <th style="text-align:center; color: white; width:50px; background-color: #2e7d32;">
