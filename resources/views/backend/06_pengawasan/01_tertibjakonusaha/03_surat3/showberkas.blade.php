dc7653dfe4a2cecf4294808c07b261fe55b4493a
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-26 01:01:42 +0700

    e

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/03_surat3/showberkas.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/03_surat3/showberkas.blade.php
@@ -349,9 +349,9 @@
                                         <tbody>
                                             <tr>
                                             <td style="border: 1px solid #000; text-align: center; height:75px;">1</td>
-                                              <td style="border: 1px solid #000;">{{$datasurat3->namabujk ?? 'Data Belum Di Buat '}}</td>
-                                              <td style="border: 1px solid #000;">{{$datasurat3->nib ?? 'Data Belum Di Buat '}}</td>
-                                              <td style="border: 1px solid #000;">{{$datasurat3->pjbu ?? 'Data Belum Di Buat '}}</td>
+                                              <td style="border: 1px solid #000; padding:0 8px;">{{$datasurat3->namabujk ?? 'Data Belum Di Buat '}}</td>
+                                              <td style="border: 1px solid #000; padding:0 8px;">{{$datasurat3->nib ?? 'Data Belum Di Buat '}}</td>
+                                              <td style="border: 1px solid #000; padding:0 8px;">{{$datasurat3->pjbu ?? 'Data Belum Di Buat '}}</td>
 
                                             </tr>
                                         </tbody>
@@ -370,8 +370,8 @@
 
                                             <tr>
                                                 <td style="border: 1px solid #000; height:75px;">{{$datasurat3->jenisusaha ?? 'Data Belum Di Buat '}}</td>
-                                                <td style="border: 1px solid #000;">{{$datasurat3->subklasifikasi->kode ?? 'Data Belum Di Buat '}} {{$datasurat3->subklasifikasi->pekerjaan ?? 'Data Belum Di Buat '}}</td>
-                                                <td style="border: 1px solid #000;">{{$datasurat3->nomorsertifikat ?? 'Data Belum Di Buat '}}</td>
+                                                <td style="border: 1px solid #000; padding:0 8px;">{{$datasurat3->subklasifikasi->kode ?? 'Data Belum Di Buat '}} {{$datasurat3->subklasifikasi->pekerjaan ?? 'Data Belum Di Buat '}}</td>
+                                                <td style="border: 1px solid #000; padding:0 8px;">{{$datasurat3->nomorsertifikat ?? 'Data Belum Di Buat '}}</td>
                                             </tr>
                                         </tbody>
                                     </table>
@@ -387,8 +387,8 @@
                                         <tbody>
 
                                             <tr>
-                                                <td style="border: 1px solid #000; height:75px;">{{$datasurat3->kesimpulan ?? 'Data Belum Di Buat '}}</td>
-                                                <td style="border: 1px solid #000;">{{$datasurat3->catatanpemeriksaan ?? 'Data Belum Di Buat '}}</td>
+                                                <td style="border: 1px solid #000; height:75px; padding:0 8px;">{{$datasurat3->kesimpulan ?? 'Data Belum Di Buat '}}</td>
+                                                <td style="border: 1px solid #000; padding:0 8px;">{{$datasurat3->catatanpemeriksaan ?? 'Data Belum Di Buat '}}</td>
                                             </tr>
                                         </tbody>
                                     </table>
