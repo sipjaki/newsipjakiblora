e9420af6af7e5601ae09f4675651bed4e6aa15a9
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-25 15:14:51 +0700

    e

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/02_surat2/index.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/02_surat2/index.blade.php
@@ -174,7 +174,7 @@
                                         <td style="text-align: left;">{{ $item->namapaketpekerjaan }}</td>
 
                                                @php
-                                                $kesesuaian = $item->kesesuaian ?? 'Surat Belum Di Buat';
+                                                $kesesuaian = $item->kesesuaiansbu ?? 'Surat Belum Di Buat';
                                                 $tertibStatus = $kesesuaian === 'Sesuai' ? 'TERTIB' : 'BELUM TERTIB';
                                                 $color = $kesesuaian === 'Sesuai' ? 'blue' : 'red';
                                                 $icon = $kesesuaian === 'Sesuai' ? 'bi-check-circle' : 'bi-x-circle';
@@ -193,7 +193,7 @@
 
 
                           @php
-                          $kesesuaian = $item->kesesuaiansbu ?? 'Surat Belum Di Buat';
+                          $kesesuaian = $item->sbu ?? 'Surat Belum Di Buat';
                           $tertibStatus = $kesesuaian === 'Sesuai' ? 'TERTIB' : 'BELUM TERTIB';
                           $color = $kesesuaian === 'Sesuai' ? 'blue' : 'red';
                           $icon = $kesesuaian === 'Sesuai' ? 'bi-check-circle' : 'bi-x-circle';
@@ -210,43 +210,6 @@
                           </button>
                       </td>
 
-
-                      @php
-                      $kesesuaian = $item->kesesuaianklasifikasi ?? 'Surat Belum Di Buat';
-                      $tertibStatus = $kesesuaian === 'Sesuai' ? 'TERTIB' : 'BELUM TERTIB';
-                      $color = $kesesuaian === 'Sesuai' ? 'blue' : 'red';
-                      $icon = $kesesuaian === 'Sesuai' ? 'bi-check-circle' : 'bi-x-circle';
-                  @endphp
-
-                  <td style="text-align: center;">
-                      <button
-                          style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer; background-color: {{ $color }};"
-                          onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                          onmouseout="this.style.backgroundColor='{{ $color }}'; this.style.color='white';"
-                      >
-                          <i class="bi {{ $icon }}" style="margin-right: 8px;"></i>
-                          {{ $tertibStatus }}
-                      </button>
-                  </td>
-
-
-                  @php
-                  $kesesuaian = $item->kesesuaianlayananusaha ?? 'Surat Belum Di Buat';
-                  $tertibStatus = $kesesuaian === 'Sesuai' ? 'TERTIB' : 'BELUM TERTIB';
-                  $color = $kesesuaian === 'Sesuai' ? 'blue' : 'red';
-                  $icon = $kesesuaian === 'Sesuai' ? 'bi-check-circle' : 'bi-x-circle';
-              @endphp
-
-              <td style="text-align: center;">
-                  <button
-                      style="padding: 8px 12px; border: none; border-radius: 5px; color: white; font-weight: bold; cursor: pointer; background-color: {{ $color }};"
-                      onmouseover="this.style.backgroundColor='white'; this.style.color='black';"
-                      onmouseout="this.style.backgroundColor='{{ $color }}'; this.style.color='white';"
-                  >
-                      <i class="bi {{ $icon }}" style="margin-right: 8px;"></i>
-                      {{ $tertibStatus }}
-                  </button>
-              </td>
 
                                         <td style="text-align: center;">
                                             <!-- Show Icon -->
