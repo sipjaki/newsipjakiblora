7abfc410b6559eada56cd541d7d4477482e8d9d8
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-25 22:54:54 +0700

    e

--- app/Models/surattertibjakonusaha3.php
+++ app/Models/surattertibjakonusaha3.php
@@ -13,10 +13,10 @@ class surattertibjakonusaha3 extends Model
 
     protected $guarded = ['id'];
 
-    public function tertibjasakonstruksi()
-    {
-        return $this->hasOne(tertibjasakonstruksi::class, 'tertibjasakonstruksi_id');
-    }
+      public function tertibjasakonstruksi()
+            {
+                return $this->hasOne(tertibjasakonstruksi::class, 'surattertibjakonusaha1_id')->oldest();
+            }
 
     public function tandatangan1()
     {
