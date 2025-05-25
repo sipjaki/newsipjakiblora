0791735043945d6cc53d71f96f780175bc674942
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-25 23:51:31 +0700

    e

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/03_surat3/showberkas.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/03_surat3/showberkas.blade.php
@@ -173,24 +173,22 @@
                                                                                                                             </tr>
                                                                                                                         </thead>
                                                                                                                         <tbody>
-                                                                                                                                <tr>
-                                                                                                                                    <td style="font-size: 12px; text-align:center;">1</td>
-                                                                                                                                        <td style="font-size: 16px;">
-                                                                                                                                            {{ optional($datasurat3->tandatangan1)->tandatangan ?? 'Belum Di Tanda Tangan' }}
-                                                                                                                                        </td>
+                                                                                                                                   <tr>
+                                                                                                                                        <td style="font-size: 12px; text-align: center;">1</td>
+                                                                                                                                        <td style="font-size: 12px;">{{$datasurat3->tandatangan1->namalengkap ?? 'Tidak Ada Tim Pemeriksa'}}</td>
+                                                                                                                                        <td style="font-size: 12px;">{{$datasurat3->tandatangan1->tandatangan ?? 'Belum Di Tanda Tangan'}}</td>
                                                                                                                                     </tr>
                                                                                                                                     <tr>
-                                                                                                                                        <td style="font-size: 12px; text-align:center;">2</td>
-                                                                                                                                        <td style="font-size: 16px;">
-                                                                                                                                            {{ optional($datasurat3->tandatangan2)->tandatangan ?? 'Belum Di Tanda Tangan' }}
-                                                                                                                                        </td>
+                                                                                                                                        <td style="font-size: 12px; text-align: center;">2</td>
+                                                                                                                                        <td style="font-size: 12px;">{{$datasurat3->tandatangan2->namalengkap ?? 'Tidak Ada Tim Pemeriksa'}}</td>
+                                                                                                                                        <td style="font-size: 12px;">{{$datasurat3->tandatangan2->tandatangan ?? 'Belum Di Tanda Tangan'}}</td>
                                                                                                                                     </tr>
                                                                                                                                     <tr>
-                                                                                                                                        <td style="font-size: 12px; text-align:center;">3</td>
-                                                                                                                                        <td style="font-size: 16px;">
-                                                                                                                                            {{ optional($datasurat3->tandatangan3)->tandatangan ?? 'Belum Di Tanda Tangan' }}
-                                                                                                                                        </td>
+                                                                                                                                        <td style="font-size: 12px; text-align: center;">3</td>
+                                                                                                                                        <td style="font-size: 12px;">{{$datasurat3->tandatangan3->namalengkap ?? 'Tidak Ada Tim Pemeriksa'}}</td>
+                                                                                                                                        <td style="font-size: 12px;">{{$datasurat3->tandatangan3->tandatangan ?? 'Belum Di Tanda Tangan'}}</td>
                                                                                                                                     </tr>
+
                                                                                                                         </tbody>
                                                                                                                     </table>
                                                                                                                 </div>
