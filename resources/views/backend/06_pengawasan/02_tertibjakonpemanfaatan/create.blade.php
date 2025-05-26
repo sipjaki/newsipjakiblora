954a7b7b91f29efa0ceb8982e7cb23122f73edf2
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-26 23:26:34 +0700

    e

--- resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/create.blade.php
+++ resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/create.blade.php
@@ -182,13 +182,7 @@
                     <div style="display: flex; justify-content: flex-end; margin-bottom:20px;">
                                     <div class="flex justify-end btn-create">
                                         <button type="button" onclick="openModal()">
-                                        <!-- Ikon SVG Pensil -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        fill="currentColor" viewBox="0 0 16 16" style="margin-right: 8px;">
-                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                   </svg>
-                                        <span style="font-family: 'Poppins', sans-serif;">Create</span>
+                                            <i class="bi bi-plus-circle-fill icon-create"></i>
                                     </button>
                                     </div>
                                     <!-- Modal Konfirmasi -->
