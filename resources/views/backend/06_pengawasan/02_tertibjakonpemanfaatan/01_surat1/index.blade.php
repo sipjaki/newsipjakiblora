cf5d26a0b963deaadedc785f3900cd2bbe13c730
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-27 00:49:37 +0700

    ergrege

--- resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/01_surat1/index.blade.php
+++ resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/01_surat1/index.blade.php
@@ -100,14 +100,14 @@
 
 
                                     {{-- @if(empty($data->surattertibjakonusaha1->id)) --}}
-{{-- @if ($datasurat->isEmpty())
+@if ($datasurat->isEmpty())
     <a href="{{ route('betertibjakonpemanfataanjakoncreateberkas', ['id' => $datasurat_id]) }}">
         <button class="btn-create">
             <i class="bi bi-file-earmark-plus icon-create"></i>
             Buat Berkas
         </button>
     </a>
-@endif --}}
+@endif
 
 
                                     {{-- @endif --}}
