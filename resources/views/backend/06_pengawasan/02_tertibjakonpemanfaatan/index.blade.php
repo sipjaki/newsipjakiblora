dcf45869ca6d237963a8639f6d48189482329c0d
SipjakiBlora <sipjakiblora@gmail.com>
2025-05-27 15:05:28 +0700

    e

--- resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/index.blade.php
+++ resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/index.blade.php
@@ -346,9 +346,17 @@
                                 <!-- Tombol Aksi -->
                                 {{-- <a href="/bebujkkonsultan/show/{{$item->namalengkap}}" class="btn btn-sm btn-info"><i class="bi bi-eye"></i></a> --}}
                                 <a href="/betertibjakonpemanfaatan/update/{{$item->id}}" class="btn btn-sm btn-warning"><i class="bi bi-pencil-square"></i></a>
-                                <a href="javascript:void(0)" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" data-judul="{{ $item->id }}" onclick="setDeleteUrl(this)">
+                                {{-- <a href="javascript:void(0)" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" data-judul="{{ $item->id }}" onclick="setDeleteUrl(this)">
                                   <i class="bi bi-trash"></i>
+                                </a> --}}
+                                <a href="javascript:void(0)" class="btn btn-sm btn-danger"
+                                data-bs-toggle="modal"
+                                data-bs-target="#deleteModal"
+                                data-url="{{ route('betertibjakonusahapemenuhansyaratdeletedata', $item->id) }}"
+                                onclick="setDeleteUrl(this)">
+                                    <i class="bi bi-trash"></i>
                                 </a>
+
                               </td>
                             </tr>
                             @endforeach
@@ -384,13 +392,13 @@
                     </div>
                 </div>
 
-                <script>
+             <script>
                 function setDeleteUrl(button) {
-                    var id = button.getAttribute('data-judul');
-                    var deleteUrl = "/betertibjakonpemanfaatandelete/delete/" + encodeURIComponent(id);
+                    var deleteUrl = button.getAttribute('data-url');
                     document.getElementById('deleteForm').action = deleteUrl;
                 }
                 </script>
+
                  <style>
                      .table-responsive {
                          max-width: 100%;
