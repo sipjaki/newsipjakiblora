8d66c0eae8f4cd38566e03b2c415994a27e4013a
SipjakiBlora <sipjakiblora@gmail.com>
2025-06-01 23:10:00 +0700

    e

--- resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/02_surat2/buatberkasbaru.blade.php
+++ resources/views/backend/06_pengawasan/02_tertibjakonpemanfaatan/02_surat2/buatberkasbaru.blade.php
@@ -213,7 +213,7 @@
         {{-- ======================================================= --}}
                     <div class="col-md-12">
                         <!--begin::Quick Example-->
-                        <form action="{{ route('betertibjakonmanfaat1') }}" method="POST" enctype="multipart/form-data">
+                        <form action="{{ route('betertibjakonmanfaat2') }}" method="POST" enctype="multipart/form-data">
                             @csrf
 
                             <input type="hidden"  name="tertibjakonpemanfaatan_id" value="{{ $datatertibjasakonstruksi_id }}">
