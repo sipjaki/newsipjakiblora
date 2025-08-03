e00c3a281445cd109a539ba1883f70f70a4810b8
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-04 00:08:07 +0700

    Create Paket Pekerjaan

--- resources/views/backend/04_datajakon/06_profilpaketpekerjaan/buatbarupaket.blade.php
+++ resources/views/backend/04_datajakon/06_profilpaketpekerjaan/buatbarupaket.blade.php
@@ -497,14 +497,13 @@ function clearFormat(input) {
             <option value="">-- Pilih Bulan --</option>
             @php
                 $months = [
-                    1 => 'Januari', 2 => 'Februari', 3 => 'Maret', 4 => 'April',
-                    5 => 'Mei', 6 => 'Juni', 7 => 'Juli', 8 => 'Agustus',
-                    9 => 'September', 10 => 'Oktober', 11 => 'November', 12 => 'Desember'
+                    'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+                    'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
                 ];
             @endphp
-            @foreach ($months as $num => $name)
-                <option value="{{ $num }}" {{ old('bulanselesai', $data->bulanselesai ?? '') == $num ? 'selected' : '' }}>
-                    {{ $name }}
+            @foreach ($months as $month)
+                <option value="{{ $month }}" {{ old('bulanselesai', $data->bulanselesai ?? '') == $month ? 'selected' : '' }}>
+                    {{ $month }}
                 </option>
             @endforeach
         </select>
