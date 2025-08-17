51c9f80d8e9b6857d8f7a6c56535b6c887131243
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-17 17:27:21 +0700

    Perubahan Style

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/04_surat4/buatberkasbaru.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/04_surat4/buatberkasbaru.blade.php
@@ -8,6 +8,7 @@
 
 @include('backend.00_administrator.00_baganterpisah.04_navbar')
 @include('backend.00_style.01_cssdashboard.style')
+@include('button')
 
 {{-- ---------------------------------------------------------------------- --}}
 
@@ -15,7 +16,8 @@
 
       <!--begin::App Main-->
       <main class="app-main">
-        <section style="background-image: url('/assets/00_android/iconmenu/menuutama.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100%; min-height: 100vh;" loading="lazy">
+        {{-- <section style="background-image: url('/assets/00_android/iconmenu/menuutama.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; width: 100%; min-height: 100vh;" loading="lazy"> --}}
+<section style="background: linear-gradient(to bottom, #a8f0c6, #ffffff); width: 100%; min-height: 100vh;">
 
         <!--begin::App Content Header-->
         <div class="app-content-header">
@@ -51,7 +53,7 @@
                                 <!-- Kontainer Kiri -->
                                 <div style="display: flex; gap: 10px; margin-right:25px; margin-left:25px;">
                                     <!-- Tombol Nama Pekerjaan -->
-                                    <button class="btn-create">
+                                    <button class="button-baru">
                                         <i class="bi bi-file-earmark" style="margin-right: 8px;"></i>
                                         Nama Pekerjaan: {{ $datatertibjasakonstruksi ?? 'Masih Kosong' }}
                                     </button>
@@ -196,7 +198,7 @@
                                 <!-- Kontainer Kanan -->
                                 <div>
                              <a href="{{ url()->previous() }}">
-                                    <button class="btn-kembali">
+                                    <button class="button-newvalidasi">
                                         <i class="bi bi-arrow-left icon-create" style="margin-right: 8px;"></i>
                                         Kembali
                                     </button>
@@ -252,6 +254,7 @@
                                                 <tr>
                                                     <th style="border: 1px solid #000; text-align: center; background-color: #f8f9fa; font-size:16px; width:50px;">No</th>
                                                     <th rowspan="2" style="border: 1px solid #000; text-align: center; background-color: #f8f9fa; color: #000; width: 400px; font-size:15px;">Nama Badan Usaha</th>
+                                                    <th style="border: 1px solid #000; text-align: center; background-color: #f8f9fa; font-size:16px;">Pelaksana Pengembangan Usaha</th>
                                                     <th style="border: 1px solid #000; text-align: center; background-color: #f8f9fa; font-size:16px;">Status Pelaksana Pengembangan Usaha</th>
                                                 </tr>
                                             </thead>
@@ -264,6 +267,17 @@
                                                         {{ $datatertibjasakonstruksinamabadanusaha ?? 'Data Masih Kosong' }}
                                                     </div>
                                                 </td>
+
+                                                <td style="border: 1px solid #000; width: 400px; vertical-align: top; padding: 8px;">
+    <div style="white-space: pre-wrap; min-height: 120px;">
+        {{ "
+1. Melakukan pengembangan sumber daya manusia agar tenaga kerja memiliki kompetensi teknis dan manajerial sesuai standar usaha jasa konstruksi.\n
+2. Meningkatkan peralatan untuk mendukung produktivitas, akurasi, dan keselamatan kerja.\n
+3. Menerapkan teknologi modern serta sistem pengawasan terintegrasi guna menjamin efisiensi dan kualitas proyek.\n
+4. Menyusun laporan keuangan BUJK yang transparan dan akuntabel sesuai ketentuan yang berlaku." }}
+    </div>
+</td>
+
 
                                                     <td style="border: 1px solid #000; width: 400px; text-align: center;">
                                                         <select class="form-control @error('status') is-invalid @enderror" name="status" style="text-align: center;">
@@ -283,11 +297,99 @@
                                 </div>
 
 
+
+                                <div class="container" style="margin-top: 10px;">
+                                    <!-- Modal Card -->
+
+                                            <div class="container" style="margin-top: 10px;">
+                                                <div class="row">
+                                                    <div class="col-md-6 ms-auto"> <!-- col 6 dan di sebelah kanan -->
+                                                        <!-- Modal Card -->
+                                                        <div class="card" style="border: 1px solid white;">
+                                                            <div class="card-body">
+                                                                <!-- Tim Pemeriksa -->
+                                                                <div class="tim-pemeriksa-container">
+                                                                    <div class="tim-pemeriksa">
+                                                                        <h6 style="font-size: 15px;">Tim Pemeriksa:</h6>
+                                                                        <table class="table table-sm">
+                                                                            <thead class="table-secondary">
+                                                                                <tr>
+                                                                                    <th style="width: 60px; font-size: 15px; text-align:center;" >No</th>
+                                                                                    <th style="text-align: center; font-size: 15px; text-align:center;">Nama Pemeriksa</th>
+                                                                                </tr>
+                                                                            </thead>
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td style="font-size: 12px; text-align:center;">1</td>
+                                                                                  <td style="font-size: 12px;">
+                                                                                    <select name="tandatangan1_id" class="form-control @error('tandatangan1_id') is-invalid @enderror">
+                                                                                        <option value="">-- Pilih Pemeriksa --</option>
+                                                                                        @foreach ($datatandatangan as $tandatangan)
+                                                                                            <option value="{{ $tandatangan->id }}"
+                                                                                                {{ old('tandatangan1_id', optional($data->surattertibjakonusaha4)->tandatangan1_id) == $tandatangan->id ? 'selected' : '' }}>
+                                                                                                {{ $tandatangan->namalengkap }}
+                                                                                            </option>
+                                                                                        @endforeach
+                                                                                    </select>
+                                                                                    @error('tandatangan1_id')
+                                                                                        <div class="invalid-feedback">{{ $message }}</div>
+                                                                                    @enderror
+                                                                                </td>
+
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td style="font-size: 12px; text-align:center;">2</td>
+                                                                                    <td style="font-size: 12px;">
+    <select name="tandatangan2_id" class="form-control @error('tandatangan2_id') is-invalid @enderror">
+        <option value="">-- Pilih Pemeriksa --</option>
+        @foreach ($datatandatangan as $tandatangan)
+            <option value="{{ $tandatangan->id }}"
+                {{ old('tandatangan2_id', optional($data->surattertibjakonusaha4)->tandatangan2_id) == $tandatangan->id ? 'selected' : '' }}>
+                {{ $tandatangan->namalengkap }}
+            </option>
+        @endforeach
+    </select>
+    @error('tandatangan2_id')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+</td>
+
+                                                                                </tr>
+                                                                                <tr>
+                                                                                    <td style="font-size: 12px; text-align:center;">3</td>
+                                                                                    <td style="font-size: 12px;">
+    <select name="tandatangan3_id" class="form-control @error('tandatangan3_id') is-invalid @enderror">
+        <option value="">-- Pilih Pemeriksa --</option>
+        @foreach ($datatandatangan as $tandatangan)
+            <option value="{{ $tandatangan->id }}"
+                {{ old('tandatangan3_id', optional($data->surattertibjakonusaha4)->tandatangan3_id) == $tandatangan->id ? 'selected' : '' }}>
+                {{ $tandatangan->namalengkap }}
+            </option>
+        @endforeach
+    </select>
+    @error('tandatangan3_id')
+        <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
+</td>
+
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
                                 <br><br>
 <!-- Tombol Submit -->
 <div style="display: flex; justify-content: flex-end; margin-bottom:20px;">
     <div class="flex justify-end">
-        <button class="btn-create" type="button" onclick="openModal()">
+        <button class="button-hijau" type="button" onclick="openModal()">
         <!-- Ikon SVG Pensil -->
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
         fill="currentColor" viewBox="0 0 16 16" style="margin-right: 8px;">
