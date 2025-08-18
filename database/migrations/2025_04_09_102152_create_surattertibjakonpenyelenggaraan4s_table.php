2db9c0bcc836d781fb3dcc29bd7a1f813f11c428
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-19 03:02:58 +0700

    Database Penambahan Kolom Umam

--- database/migrations/2025_04_09_102152_create_surattertibjakonpenyelenggaraan4s_table.php
+++ database/migrations/2025_04_09_102152_create_surattertibjakonpenyelenggaraan4s_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('surattertibjakonpenyelenggaraan4s', function (Blueprint $table) {
             $table->id();
             $table->text('lingkuppengawasan')->nullable()->index();
