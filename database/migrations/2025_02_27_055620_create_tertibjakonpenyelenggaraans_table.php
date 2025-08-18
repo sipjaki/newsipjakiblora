cdc31ebb274d234cbdb85949abb2034135ca87e6
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-19 04:26:22 +0700

    Database Penambahan kolom baru

--- database/migrations/2025_02_27_055620_create_tertibjakonpenyelenggaraans_table.php
+++ database/migrations/2025_02_27_055620_create_tertibjakonpenyelenggaraans_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        
         Schema::create('tertibjakonpenyelenggaraans', function (Blueprint $table) {
             $table->id();
             $table->foreignId('penyediastatustertibjakon_id')->nullable();
