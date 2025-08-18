4909efd88f435bec27b9520dce342aa73c730ab4
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-18 15:44:08 +0700

    Penambahan Relasi Baru

--- app/Models/surattertibjakonpenyelenggaraan1.php
+++ app/Models/surattertibjakonpenyelenggaraan1.php
@@ -15,7 +15,24 @@ class surattertibjakonpenyelenggaraan1 extends Model
 
     public function tertibjakonpenyelenggaraan()
     {
-        return $this->hasOne(tertibjakonpenyelenggaraan::class);
+        return $this->belongsTo(tertibjakonpenyelenggaraan::class, 'tertibjakonpenyelenggaraan_id');
     }
+
+
+        public function tandatangan1()
+    {
+        return $this->belongsTo(tandatangan::class, 'tandatangan1_id');
+    }
+
+    public function tandatangan2()
+    {
+        return $this->belongsTo(tandatangan::class, 'tandatangan2_id');
+    }
+
+    public function tandatangan3()
+    {
+        return $this->belongsTo(tandatangan::class, 'tandatangan3_id');
+    }
+
 
 }
