00a8260a2b373b2526988bf6c9c139fc88d61f29
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-22 20:27:39 +0700

    Pergantian Penamaan Sektor

--- resources/views/backend/06_pengawasan/01_tertibjakonusaha/create.blade.php
+++ resources/views/backend/06_pengawasan/01_tertibjakonusaha/create.blade.php
@@ -217,10 +217,10 @@ style="
 <div class="col-md-6">
 <div class="mb-3">
     <label for="penyediastatustertibjakon_id" class="form-label">
-        <i class="bi bi-card-text" style="color: #800000;"></i> Status Penyedia
+        <i class="bi bi-card-text" style="color: #800000;"></i> Sektor
     </label>
     <select id="penyediastatustertibjakon_id" name="penyediastatustertibjakon_id" class="form-control @error('penyediastatustertibjakon_id') is-invalid @enderror">
-        <option value="">-- Pilih Status Penyedia --</option>
+        <option value="">-- Pilih Sektor --</option>
         @foreach ($datapenyedia as $penyedia)
             <option value="{{ $penyedia->id }}" {{ (old('penyediastatustertibjakon_id', $data->penyediastatustertibjakon_id ?? '') == $penyedia->id) ? 'selected' : '' }}>
                 {{ $penyedia->penyedia }}
