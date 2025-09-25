375c7bc2fe26026184e4196184c0ec621f34d248
SipjakiBlora <sipjakiblora@gmail.com>
2025-09-25 21:35:26 +0700

    perubahan baru

--- app/Models/dokumentasijakon.php
+++ app/Models/dokumentasijakon.php
@@ -11,5 +11,23 @@ class dokumentasijakon extends Model
 {
      use HasFactory, SoftDeletes, HasApiTokens;
 
-    protected $guarded = ['id'];
+    // protected $guarded = ['id'];
+
+     protected $table = 'dokumentasijakon'; // 👈 tambahkan ini
+    protected $fillable = [
+        'judul',
+        'fotokegiatan1',
+        'fotokegiatan2',
+        'fotokegiatan3',
+        'fotokegiatan4',
+        'fotokegiatan5',
+        'fotokegiatan6',
+        'fotokegiatan7',
+        'fotokegiatan8',
+        'fotokegiatan9',
+        'fotokegiatan10',
+        'fotokegiatan11',
+        'fotokegiatan12',
+    ];
+    // protected $table = 'dokumentasijakon';
 }
